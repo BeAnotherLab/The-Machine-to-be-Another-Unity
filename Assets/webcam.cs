7a2488d3e3e544ca1834056381cd8a93ac7b8c2b
ReallyRad <arthur.pointeau@gmail.com>
2015-12-11 19:20:56 +0100

    added webcam script

---
+++ Assets/webcam.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class webcam : MonoBehaviour {
+
+	public MeshRenderer[] UseWebcamTexture;
+	WebCamTexture camTex;
+
+	// Use this for initialization
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		for (int i = 0; i < devices.Length; i++)
+		{
+			Debug.Log("Device:" + devices[i].name + " | IS FRONT FACING:" + devices[i].isFrontFacing);
+		}
+
+		camTex = new WebCamTexture(devices[0].name, 10000, 10000, 60);
+		camTex.Play();
+
+		foreach(MeshRenderer r in UseWebcamTexture)
+		{
+			r.material.mainTexture = camTex;
+			Debug.Log("hi");
+		}		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
