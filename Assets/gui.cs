559a06ecdc0733745f9a2e490e7333da54be9868
ReallyRad <arthur.pointeau@gmail.com>
2015-12-15 14:07:24 +0100

    show/hide GUI

---
+++ Assets/gui.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gui : MonoBehaviour {
+	
+	public GameObject panel;
+	private bool monitorGUIEnabled;
+
+	// Use this for initialization
+	void Start () {
+		monitorGUIEnabled = true;
+	}
+
+	public void setMonitorGUIEnabled() {
+		monitorGUIEnabled = !monitorGUIEnabled;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		panel.SetActive (monitorGUIEnabled);
+		/*for (int i = 0; i < canvas.childCount; i++) 
+		{
+			
+		}*/
+		//canvas.chi
+	}
+
+}
