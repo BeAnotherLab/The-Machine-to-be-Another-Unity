526a6a3b8cf615ef0669f3b07df1db63d68c77c6
ReallyRad <arthur.pointeau@gmail.com>
2016-01-12 19:18:24 +0100

    added arduino control sketch

---
+++ ArduinoControl/ArduinoControl.ino
@@ -0,0 +1,69 @@
+#include <SoftwareSerial.h>   // We need this even if we're not using a SoftwareSerial object
+                              // Due to the way the Arduino IDE compiles
+#include <SerialCommand.h>
+#include <Servo.h>
+
+#define arduinoLED 13   // Arduino LED on board
+
+Servo yawServo, pitchServo;  // create servo objects
+SerialCommand SCmd;   // The demo SerialCommand object
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  yawServo.attach(5);  
+  pitchServo.attach(6);
+
+  SCmd.addCommand("Pitch",pitchCommand);  // Converts two arguments to integers and echos them back 
+  SCmd.addCommand("Yaw",yawCommand);  // Converts two arguments to integers and echos them back   
+
+  pitchServo.write(90);
+  yawServo.write(90);
+}
+
+void loop () {
+  if (Serial.available() > 0)  SCmd.readSerial();   // We don't do much, just process serial commands
+}
+
+
+void pitchCommand() {
+  servoCommand(pitchServo); 
+   Serial.println("We're in pitchCommand");  
+}
+
+void yawCommand() {
+  servoCommand(yawServo);  
+}
+
+void servoCommand(Servo servo)    
+{
+  int aNumber;  
+  char *arg; 
+
+  Serial.println("We're in process_command"); 
+  arg = SCmd.next(); 
+  if (arg != NULL) 
+  {
+    aNumber=atoi(arg);    // Converts a char string to an integer
+    Serial.print("First argument was: "); 
+    Serial.println(aNumber); 
+    servo.write(aNumber);
+  } 
+  else {
+    Serial.println("No arguments"); 
+  }
+
+  arg = SCmd.next(); 
+  if (arg != NULL) 
+  {
+    aNumber=atol(arg); 
+    Serial.print("Second argument was: "); 
+    Serial.println(aNumber);     
+  } 
+  else {
+    Serial.println("No second argument"); 
+  }
+
+}
+
