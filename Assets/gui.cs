6b1e1aff9ad930aa060ed061280a45ee8bba9296
ReallyRad <arthur.pointeau@gmail.com>
2016-01-15 17:23:52 +0100

    compensate roll rotation

--- Assets/gui.cs
+++ Assets/gui.cs
@@ -31,55 +31,14 @@ public class gui : MonoBehaviour {
 	public void setYawOffset() {
 	}
 
-	public Vector3 toEulerAngles(Quaternion q)
-	{
-		// Store the Euler angles in radians
-		Vector3 pitchYawRoll = new Vector3();
-
-		float sqw = q.w * q.w;
-		float sqx = q.x * q.x;
-		float sqy = q.y * q.y;
-		float sqz = q.z * q.z;
-
-		// If quaternion is normalised the unit is one, otherwise it is the correction factor
-		float unit = sqx + sqy + sqz + sqw;
-		float test = q.x * q.y + q.z * q.w;
-
-		if (test > 0.4999f * unit)                              // 0.4999f OR 0.5f - EPSILON
-		{
-			// Singularity at north pole
-			pitchYawRoll.y = 2f * (float)Mathf.Atan2(q.x, q.w);  // Yaw
-			pitchYawRoll.x = Mathf.PI * 0.5f;                         // Pitch
-			pitchYawRoll.z = 0f;                                // Roll
-			return pitchYawRoll;
-		}
-		else if (test < -0.4999f * unit)                        // -0.4999f OR -0.5f + EPSILON
-		{
-			// Singularity at south pole
-			pitchYawRoll.y = -2f * (float)Mathf.Atan2(q.x, q.w); // Yaw
-			pitchYawRoll.x = -Mathf.PI * 0.5f;                        // Pitch
-			pitchYawRoll.z = 0f;                                // Roll
-			return pitchYawRoll;
-		}
-		else
-		{
-			pitchYawRoll.y = (float)Mathf.Atan2(2f * q.y * q.w - 2f * q.x * q.z, sqx - sqy - sqz + sqw) * 180/Mathf.PI;       // Yaw
-			pitchYawRoll.x = (float)Mathf.Asin(2f * test / unit) * 180/Mathf.PI;                                              // Pitch
-			pitchYawRoll.z = (float)Mathf.Atan2(2f * q.x * q.w - 2f * q.y * q.z, -sqx + sqy - sqz + sqw) * 180/Mathf.PI;      // Roll
-		}
-
-		return pitchYawRoll;
-	}
-
-
 	// Update is called once per frame
 	void Update () {
 		panel.SetActive (monitorGUIEnabled);
 		if (webCamDisplay.GetComponent<webcam>().isHeadtrackingOn ()) {		
-			Vector3 pitchYawRoll = toEulerAngles (mainCamera.transform.rotation);
-			pitchSlider.value = pitchYawRoll.z + 90;
-			yawSlider.value = 90 - pitchYawRoll.y;
+			Vector3 pitchYawRoll = webCamDisplay.GetComponent<webcam>().toEulerAngles (mainCamera.transform.rotation);
 			rollSlider.value = pitchYawRoll.x;
+			yawSlider.value = 90 - pitchYawRoll.y;
+			pitchSlider.value = pitchYawRoll.z + 90;
 		}
 				
 	}
--- Assets/webcam.cs
+++ Assets/webcam.cs
@@ -70,10 +70,52 @@ public class webcam : MonoBehaviour {
 		headtrackingOn = !headtrackingOn;
 	}
 
+
+	public Vector3 toEulerAngles(Quaternion q)
+	{
+		// Store the Euler angles in radians
+		Vector3 pitchYawRoll = new Vector3();
+
+		float sqw = q.w * q.w;
+		float sqx = q.x * q.x;
+		float sqy = q.y * q.y;
+		float sqz = q.z * q.z;
+
+		// If quaternion is normalised the unit is one, otherwise it is the correction factor
+		float unit = sqx + sqy + sqz + sqw;
+		float test = q.x * q.y + q.z * q.w;
+
+		if (test > 0.4999f * unit)                              // 0.4999f OR 0.5f - EPSILON
+		{
+			// Singularity at north pole
+			pitchYawRoll.y = 2f * (float)Mathf.Atan2(q.x, q.w);  // Yaw
+			pitchYawRoll.x = Mathf.PI * 0.5f;                         // Pitch
+			pitchYawRoll.z = 0f;                                // Roll
+			return pitchYawRoll;
+		}
+		else if (test < -0.4999f * unit)                        // -0.4999f OR -0.5f + EPSILON
+		{
+			// Singularity at south pole
+			pitchYawRoll.y = -2f * (float)Mathf.Atan2(q.x, q.w); // Yaw
+			pitchYawRoll.x = -Mathf.PI * 0.5f;                        // Pitch
+			pitchYawRoll.z = 0f;                                // Roll
+			return pitchYawRoll;
+		}
+		else
+		{
+			pitchYawRoll.y = (float)Mathf.Atan2(2f * q.y * q.w - 2f * q.x * q.z, sqx - sqy - sqz + sqw) * 180/Mathf.PI;       // Yaw
+			pitchYawRoll.x = (float)Mathf.Asin(2f * test / unit) * 180/Mathf.PI;                                              // Pitch
+			pitchYawRoll.z = (float)Mathf.Atan2(2f * q.x * q.w - 2f * q.y * q.z, -sqx + sqy - sqz + sqw) * 180/Mathf.PI;      // Roll
+		}
+
+		return pitchYawRoll;
+	}
+
 	// Update is called once per frame
 	void Update () {		
 		transform.position = POVCamera.transform.position + POVCamera.transform.forward * 15; //keep webcam at a certain distance from head.
 		transform.rotation = POVCamera.transform.rotation; //keep webcam feed aligned with head
+		transform.rotation *= Quaternion.Euler (0, 0, 1) * Quaternion.AngleAxis(-toEulerAngles(POVCamera.transform.rotation).x, Vector3.forward); //compensate for absence of roll servo
 		transform.rotation *= Quaternion.Euler (0, 0, tiltAngle) * Quaternion.AngleAxis(camTex.videoRotationAngle, Vector3.up); //to adjust for webcam physical orientation
 		transform.localScale = new Vector3 (width/height*zoom, height/width*zoom, 0);
 		setDimLevel ();
