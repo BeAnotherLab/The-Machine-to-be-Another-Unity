453218d514599d4d5748059c65563dd7fad282e8
ReallyRad <arthur.pointeau@gmail.com>
2016-08-02 13:38:36 +0200

    added ovrvision camera package

---
+++ Assets/OvrvisionPro/Demos/CamImageCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamImageCube : MonoBehaviour {
+
+	private Ovrvision ovrObj = null;
+
+	// Use this for initialization
+	void Start () {
+		ovrObj = GameObject.Find("OvrvisionProCamera").GetComponent<Ovrvision>();
+		this.GetComponent<Renderer>().material.mainTexture = ovrObj.GetCameraTextureLeft();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Texture2D leftimage = ovrObj.GetCameraTextureLeft();
+		this.GetComponent<Renderer>().material.mainTexture = leftimage;
+
+		//Debug.Log(leftimage.GetPixel(100, 100).ToString()); //Debug
+	}
+}
