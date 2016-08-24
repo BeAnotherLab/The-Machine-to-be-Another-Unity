88b663d28ec837d6367214f7c1f25bd7b5dc668d
ReallyRad <arthur.pointeau@gmail.com>
2016-08-24 13:04:10 +0200

    added setserialdropdownoptions back

--- Assets/Scripts/gui.cs
+++ Assets/Scripts/gui.cs
@@ -6,22 +6,35 @@ using System.IO.Ports;
 public class gui : MonoBehaviour {
 	
 	public GameObject panel, webCamDisplay, mainCamera;
+	public Slider pitchSlider, yawSlider, rollSlider, zoomSlider;
+	public Dropdown serialDropdown, cameraDropdown;
+
+	private bool twoWaySwap = true;
 	private bool monitorGUIEnabled, oculusGUIEnabled;
 	private int zoom;
 	private int camera_orientation;	
 	private int camera_id;
-	public Slider pitchSlider, yawSlider, rollSlider, zoomSlider;
-	public Dropdown cameraDropdown;
-
 	// Use this for initialization
 	void Start () {
 		monitorGUIEnabled = true;
 		setCameraDropdownOptions ();
+		setSerialPortDropdownOptions();
 	}
 
 	public void setMonitorGUIEnabled() {
 		monitorGUIEnabled = !monitorGUIEnabled;
 	}		
+
+	private void setSerialPortDropdownOptions() {
+		if (!twoWaySwap) {
+			string[] ports = SerialPort.GetPortNames ();
+			serialDropdown.options.Clear ();
+			foreach (string c in ports) {
+				serialDropdown.options.Add (new Dropdown.OptionData () { text = c });
+			}
+			serialDropdown.value = PlayerPrefs.GetInt ("Serial port");
+		}
+	}
 
 	private void setCameraDropdownOptions(){
 		WebCamDevice[] devices = WebCamTexture.devices;
@@ -29,7 +42,7 @@ public class gui : MonoBehaviour {
 		foreach(WebCamDevice device in devices){
 			cameraDropdown.options.Add (new Dropdown.OptionData () { text = device.name });			
 		}
-		//cameraDropdown.value = PlayerPrefs.GetInt ("cameraID");
+		cameraDropdown.value = PlayerPrefs.GetInt ("cameraID");
 
 	}
 		
