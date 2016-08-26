a1872ef0106d26fbe3276632c99bf65f1b89cae6
ReallyRad <arthur.pointeau@gmail.com>
2016-08-26 18:58:53 +0200

    move plane forward

--- Assets/Scripts/webcam.cs
+++ Assets/Scripts/webcam.cs
@@ -110,7 +110,7 @@ public class webcam : MonoBehaviour {
 			//transform.localScale = new Vector3 (width/height*zoom, height/width*zoom, 0);
 			transform.localScale = new Vector3 (widthHeightRatio * zoom, 1 / widthHeightRatio * zoom, 0);
 		} else {
-			transform.position = otherPosition; //+ otherPose * Vector3.forward * 15; //keep webcam at a certain distance from head.
+			transform.position = otherPosition + otherPose * Vector3.forward * 15; //keep webcam at a certain distance from head.
 			transform.rotation = otherPose; //keep webcam feed aligned with head
 			//transform.rotation *= Quaternion.Euler (0, 0, 1) * Quaternion.AngleAxis (-utilities.toEulerAngles (POVCamera.transform.rotation).x, Vector3.forward); //compensate for absence of roll servo
 			transform.rotation *= Quaternion.Euler (0, 0, tiltAngle) * Quaternion.AngleAxis (camTex.videoRotationAngle, Vector3.up); //to adjust for webcam physical orientation
