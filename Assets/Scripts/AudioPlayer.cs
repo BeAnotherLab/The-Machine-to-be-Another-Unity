f1ef96148a2d16a65e0bd93c374b75c1260ac9c4
ReallyRad <arthur.pointeau@gmail.com>
2019-02-05 14:53:42 +0100

    Reformated Audio Player script and audio manager prefab to play audios the way it was in the classic swap

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -5,75 +5,96 @@ using System.IO;
 
 public class AudioPlayer : MonoBehaviour {
 
-	public AudioSource[] clips;
-	public AudioSource music;
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    [SerializeField]
+    private AudioSource[] _clips; //audios to be triggered with keys or touchOSC
+    [SerializeField]
+    private AudioSource _music; //the background music
+
+    private bool _somethingIsPlaying;
+
+    #endregion
+
+    #region MonoBehaviour Methods
+
+    private void Awake()
+    {
+        _clips = transform.Find("English audios").GetComponentsInChildren<AudioSource>();
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        //play looping background music
+        _music.loop = true;
+        _music.Play();
+        foreach (AudioSource clip in _clips)
+            clip.Pause();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKey(vKey))
+            {
+                //your code here
+                if (vKey == UnityEngine.KeyCode.Q)
+                    playSound(0);
+                else if (vKey == UnityEngine.KeyCode.W)
+                    playSound(1);
+                else if (vKey == UnityEngine.KeyCode.E)
+                    playSound(2);
+                else if (vKey == UnityEngine.KeyCode.R)
+                    playSound(3);
+                else if (vKey == UnityEngine.KeyCode.T)
+                    playSound(4);
+                else if (vKey == UnityEngine.KeyCode.Y)
+                    playSound(5);
+                else if (vKey == UnityEngine.KeyCode.U)
+                    playSound(6);
+                else if (vKey == UnityEngine.KeyCode.I)
+                    playSound(7);
+            }
+        }
+
+        _somethingIsPlaying = false;
+
+        //check if some audio is playing 
+        for (int i = 0; i < _clips.Length; i++)
+        {
+            if (_clips[i].isPlaying)
+                _somethingIsPlaying = true;
+        }
+
+        if (!_somethingIsPlaying)
+            _music.volume = 1;
+
+    }
+
+    #endregion
 
 
-	private bool somethingIsPlaying;
+    #region Public Methods
 
-	// Use this for initialization
-	void Start () {
-		//play looping background music
-		music.loop = true;
-		music.Play();
-		foreach (AudioSource clip in clips) {
-			clip.Pause ();
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		foreach(KeyCode vKey in System.Enum.GetValues(typeof(KeyCode))){
-			if(Input.GetKey(vKey)){
-				//your code here
-				if (vKey == UnityEngine.KeyCode.Q)
-					playSound (0);
-				else if (vKey == UnityEngine.KeyCode.W)
-					playSound (1);
-				else if (vKey == UnityEngine.KeyCode.E)
-					playSound (2);
-				else if (vKey == UnityEngine.KeyCode.R)
-					playSound (3);
-				else if (vKey == UnityEngine.KeyCode.T)
-					playSound (4);
-				else if (vKey == UnityEngine.KeyCode.Y)
-					playSound (5);
-				else if (vKey == UnityEngine.KeyCode.U)
-					playSound (6);
-				else if (vKey == UnityEngine.KeyCode.I)
-					playSound (7);
-			}
-		}
-
-		somethingIsPlaying = false;
-
-		//check if some audio is playing 
-		for (int i = 0; i < clips.Length; i++) {
-			if (clips [i].isPlaying)
-				somethingIsPlaying = true;
-		}
-
-		if (!somethingIsPlaying)
-			music.volume = 1;
-				
-	}
-
-	public void playSound(int id){
-		if (!somethingIsPlaying) {
-			Debug.Log ("playing sound" + id.ToString ());
-			clips [id].Play ();
-			music.volume = 0.45f;
-		}
-	}
-
-	/*
-	 * */
-//assign a TouchOSC control
-//assign a keyboard key for each sound
-//show on GUI what key triggers what sound
-//prevent from playing if one is already playing
+    public void playSound(int id)
+    {
+        if (!_somethingIsPlaying)
+        {
+            Debug.Log("playing sound" + id.ToString());
+            _clips[id].Play();
+            _music.volume = 0.45f;
+        }
+    }
+    #endregion
 
 
+    #region Private Methods
+    #endregion
 
 }
