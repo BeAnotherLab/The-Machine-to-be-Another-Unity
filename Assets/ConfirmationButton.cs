64a07d87c94b69458d5f38fff94570c2013023e8
ReallyRad <arthur.pointeau@gmail.com>
2019-02-14 17:56:16 +0100

    [SAVE SCENE] add VR input abilities to Main Camera, selection radial animation, plane pops out when gazed at.

---
+++ Assets/ConfirmationButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Menu
+{
+    // This script is for loading scenes from the main menu.
+    // Each 'button' will be a rendering showing the scene
+    // that will be loaded and use the SelectionRadial.
+    public class ConfirmationButton : MonoBehaviour
+    {
+        public event Action<ConfirmationButton> OnButtonSelected;           // This event is triggered when the selection of the button has finished.
+
+        [SerializeField] private SelectionRadial m_SelectionRadial;         // This controls when the selection is complete.
+        [SerializeField] private VRInteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
+
+        private bool m_GazeOver;                                            // Whether the user is looking at the VRInteractiveItem currently.
+
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnOver += HandleOver;
+            m_InteractiveItem.OnOut += HandleOut;
+            m_SelectionRadial.OnSelectionComplete += HandleSelectionComplete;
+        }
+
+        private void OnDisable()
+        {
+            m_InteractiveItem.OnOver -= HandleOver;
+            m_InteractiveItem.OnOut -= HandleOut;
+            m_SelectionRadial.OnSelectionComplete -= HandleSelectionComplete;
+        }
+
+        private void HandleOver()
+        {
+            // When the user looks at the rendering of the scene, show the radial.
+            m_SelectionRadial.Show();
+
+            m_GazeOver = true;
+        }
+
+        private void HandleOut()
+        {
+            // When the user looks away from the rendering of the scene, hide the radial.
+            m_SelectionRadial.Hide();
+
+            m_GazeOver = false;
+        }
+
+        private void HandleSelectionComplete()
+        {
+            //the user is ready
+        }
+
+    }
+}
