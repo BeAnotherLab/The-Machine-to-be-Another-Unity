5e0398b45149a5fcb72247b4e98b803c0eae0a5e
ReallyRad <arthur.pointeau@gmail.com>
2019-02-15 11:08:15 +0100

    re added marte's modifications as custom selection radial

--- Assets/ConfirmationButton.cs
+++ Assets/ConfirmationButton.cs
@@ -15,7 +15,7 @@ namespace VRStandardAssets.Menu
 
         public event Action<ConfirmationButton> OnButtonSelected;           // This event is triggered when the selection of the button has finished.
 
-        [SerializeField] private SelectionRadial m_SelectionRadial;         // This controls when the selection is complete.
+        [SerializeField] private CustomSelectionRadial m_SelectionRadial;         // This controls when the selection is complete.
         [SerializeField] private VRInteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
 
         private bool m_GazeOver;                                            // Whether the user is looking at the VRInteractiveItem currently.
