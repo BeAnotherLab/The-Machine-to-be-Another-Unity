03bf96bc16cf983e7cac0d14272315b51d307142
SCCTechnorama <60736846+SCCTechnorama@users.noreply.github.com>
2020-03-13 10:30:19 +0100

    update error discriptions

--- Arduino/Controllino/MachinToBeAnother/src/error.cpp
+++ Arduino/Controllino/MachinToBeAnother/src/error.cpp
@@ -8,19 +8,19 @@ void error(byte ErrorNr){
   switch (ErrorNr) {
 
     case 0:
-      Serial.println("sys_rdy");
+      Serial.println("sys_rdy");  //command executed
     break;
 
     case 1:
-      Serial.println("TIMEOUT");
+      Serial.println("TIMEOUT");  //endpoints of the wall not reached
     break;
 
     case 2:
-      Serial.println("MD_FAULT");
+      Serial.println("MD_FAULT"); //fault on machinery drive
     break;
 
     case 3:
-      Serial.println("MD_BLOCK");
+      Serial.println("MD_BLOCK"); //machinery drive not ready
     break;
   }
 }
