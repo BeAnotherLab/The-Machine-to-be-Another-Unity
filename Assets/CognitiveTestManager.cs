cc2297955effb6c6c520db7d05baaeea922badc9
ReallyRad <arthur.pointeau@gmail.com>
2020-03-16 12:43:57 +0100

    Crreate cogntive test scene, manager and GUI prefab

---
+++ Assets/CognitiveTestManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CognitiveTestManager : MonoBehaviour
+{
+     [SerializeField] Text _instructionsText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
