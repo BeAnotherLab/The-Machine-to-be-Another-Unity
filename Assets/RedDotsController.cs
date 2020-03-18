6856aa7cdf14e4a96cfe71f23e1419b1e4fa59e5
ReallyRad <arthur.pointeau@gmail.com>
2020-03-18 15:49:30 +0100

    read from JSON and display red dots accordingly

---
+++ Assets/RedDotsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedDotsController : MonoBehaviour
+{
+    public static RedDotsController instance;
+
+    [SerializeField] private Transform _leftSpheres;
+    [SerializeField] private Transform _rightSpheres;
+    
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public void Show(string configuration)
+    {
+        int selfSpheres = (int) Char.GetNumericValue(configuration.ToCharArray()[1]);
+        int otherSpheres = (int) Char.GetNumericValue(configuration.ToCharArray()[4]);
+        char directionFacing = configuration.ToCharArray()[7];
+        Transform visibleSide, otherSide;
+
+        if (directionFacing == 'L')
+        {
+            visibleSide = _leftSpheres;
+            otherSide = _rightSpheres;
+        }
+        else
+        {
+            visibleSide = _rightSpheres;
+            otherSide = _leftSpheres;
+        }
+            
+        DisplaySpheresForSelfOrOther(otherSpheres, otherSide);
+        DisplaySpheresForSelfOrOther(selfSpheres - otherSpheres, visibleSide);
+    }
+
+    private void DisplaySpheresOnSide(Transform side, bool top, bool center, bool bottom)
+    {
+        side.GetComponentsInChildren<MeshRenderer>()[0].enabled = top;
+        side.GetComponentsInChildren<MeshRenderer>()[1].enabled = center;
+        side.GetComponentsInChildren<MeshRenderer>()[2].enabled = bottom;
+    }
+
+    private void DisplaySpheresForSelfOrOther(int numberSpheres, Transform side)
+    {
+        if(numberSpheres == 0) DisplaySpheresOnSide(side, false, false, false);
+        if (numberSpheres == 1) DisplaySpheresOnSide(side, false, true, false);
+        else if (numberSpheres == 2) DisplaySpheresOnSide(side, true, false, true);
+        else if (numberSpheres == 3) DisplaySpheresOnSide(side, true, true, true);
+    }
+    
+}
--- Assets/Scripts/CognitiveTestManager.cs
+++ Assets/Scripts/CognitiveTestManager.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.UI;
+using System.IO;
 
 public class CognitiveTestManager : MonoBehaviour
 {
+    private string _path;
+    private string _pronoun;
+    private int _trialIndex;
 
-    public static CognitiveTestManager instance;
+    private JSONObject _trials;
+    private List<JSONObject> _practiceTrials;
+    private List<JSONObject> _testTrials1, _testTrials2, _testTrials3, _testTrials4;
+    private JSONObject _finalTrialsList;
     
-     [SerializeField] Text _instructionsText;
+    public static CognitiveTestManager instance;
+
+    [SerializeField] Text _trialInstructionText;
 
      private enum steps
      {
@@ -41,6 +48,7 @@ public class CognitiveTestManager : MonoBehaviour
         _testTrials2 = new List<JSONObject>();
         _testTrials3 = new List<JSONObject>();
         _testTrials4 = new List<JSONObject>();
+        _finalTrialsList = new JSONObject();
         
         for (int i = 0; i <= 26; i++) _practiceTrials.Add(_trials.list[i]);
         for (int i = 26; i < 78; i++) _testTrials1.Add(_trials.list[i]);
@@ -48,13 +56,17 @@ public class CognitiveTestManager : MonoBehaviour
         for (int i = 130; i < 182; i++) _testTrials3.Add(_trials.list[i]);
         for (int i = 182; i < 234; i++) _testTrials4.Add(_trials.list[i]);
         
-_
         ListExtensions.Shuffle(_practiceTrials);
         ListExtensions.Shuffle(_testTrials1);
         ListExtensions.Shuffle(_testTrials2);
         ListExtensions.Shuffle(_testTrials3);
         ListExtensions.Shuffle(_testTrials4);
         
+        foreach (JSONObject jsonObject in _testTrials4) _finalTrialsList.Add(jsonObject);
+        foreach (JSONObject jsonObject in _testTrials3) _finalTrialsList.Add(jsonObject);
+        foreach (JSONObject jsonObject in _testTrials2) _finalTrialsList.Add(jsonObject);
+        foreach (JSONObject jsonObject in _testTrials1) _finalTrialsList.Add(jsonObject);
+        foreach (JSONObject jsonObject in _practiceTrials) _finalTrialsList.Add(jsonObject);
     }
 
     // Update is called once per frame
@@ -68,14 +80,42 @@ _
 
     public void StartTest(string pronoun, string subjectID)
     {
-        Debug.Log("starting test for " + pronoun + " with subjectID " + subjectID);
+        _pronoun = pronoun;        
         CognitiveTestInstructionsGUIBehavior.instance.Init();
         _currentStep = steps.instructions;
         CognitiveTestSettingsGUI.instance.gameObject.SetActive(false);
+        _trialIndex = 0;
+        StartCoroutine(ShowTrialCoroutine());
     }
 
     public void StartTraining()
     {
         _currentStep = steps.training;
     }
+
+    private IEnumerator ShowTrialCoroutine()
+    {
+        _trialInstructionText.text = "+";
+
+        yield return new WaitForSeconds(2);
+
+        _trialInstructionText.text = _finalTrialsList[_trialIndex].GetField("stim1").str;
+        
+        yield return new WaitForSeconds(2);
+        
+        VideoFeed.instance.SetDimmed(false);
+        
+        VideoFeed.instance.FlipHorizontal(); //if need to change direction
+
+        RedDotsController.instance.Show(_finalTrialsList[_trialIndex].GetField("stim2").str);
+
+        yield return new WaitForSeconds(4);
+        
+        _trialIndex++;
+
+        StartCoroutine(ShowTrialCoroutine());
+    }
+    
+
 }
+
