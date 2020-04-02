4fd06dfd6b275604e998b3c3176cf5cdf885fa28
ReallyRad <arthur.pointeau@gmail.com>
2020-04-02 14:17:41 +0100

    add Intervention manager and intervention timeline to Spark experiment scene

---
+++ Assets/InterventionManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterventionManager : MonoBehaviour
+{
+    public static InterventionManager instance;
+
+    public enum ParticipantType { leader, follower };
+    public ParticipantType participantType;
+    
+    public enum ExperimentType { control, experimental };
+    public ExperimentType experimentType;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    public void StartInterventon()
+    {
+        //enable sending/receiving headtracking
+        OscManager.instance.sendHeadTracking = true;
+        
+        //start timing experience
+    }
+
+    public void EndIntervention()
+    {
+        OscManager.instance.sendHeadTracking = false;
+        
+        //run tests second time, skip practice 
+        CognitiveTestManager.instance.StartTest(CognitiveTestManager.steps.testing);
+    }
+
+}
