bedcf166e63d39d9e4c1c99357d885d22a728efc
ReallyRad <arthur.pointeau@gmail.com>
2020-04-03 19:13:50 +0100

    [SAVE SCENE] add second display with video players for "welcome" and "wait for turn"

---
+++ Assets/InstructionsDisplay.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class InstructionsDisplay : MonoBehaviour
+{
+    [SerializeField] VideoPlayer _welcomeLoopVideo;
+    [SerializeField] VideoPlayer _waitForTurnLoopVideo;
+}
