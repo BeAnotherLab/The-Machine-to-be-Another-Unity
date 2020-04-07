5666e0a3479c48c079658244ce9eae33c6305906
ReallyRad <arthur.pointeau@gmail.com>
2020-04-07 17:32:43 +0100

    [SAVE SCENE] add localization tools

---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LocalizationManager : MonoBehaviour {
+
+    public static LocalizationManager instance;
+
+    [SerializeField] private string[] localizationTexts;
+    
+    private Dictionary<string, string> localizedText;
+    private bool isReady = false;
+    private string missingTextString = "Localized text not found";
+
+    // Use this for initialization
+    void Awake () 
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy (gameObject);
+        }
+
+        DontDestroyOnLoad (gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("z")) LoadLocalizedText(localizationTexts[0]);
+        if (Input.GetKeyDown("x")) LoadLocalizedText(localizationTexts[1]);
+        if (Input.GetKeyDown("c")) LoadLocalizedText(localizationTexts[2]);
+        if (Input.GetKeyDown("v")) LoadLocalizedText(localizationTexts[3]);
+    }
+
+    public void LoadLocalizedText(string fileName)
+    {
+        localizedText = new Dictionary<string, string> ();
+        string filePath = Path.Combine (Application.streamingAssetsPath, fileName);
+
+        if (File.Exists (filePath)) {
+            string dataAsJson = File.ReadAllText (filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
+
+            for (int i = 0; i < loadedData.items.Length; i++) 
+            {
+                localizedText.Add (loadedData.items [i].key, loadedData.items [i].value);    
+            }
+
+            Debug.Log ("Data loaded, dictionary contains: " + localizedText.Count + " entries");
+        } else 
+        {
+            Debug.LogError ("Cannot find file!");
+        }
+
+        isReady = true;
+    }
+
+    public string GetLocalizedValue(string key)
+    {
+        string result = missingTextString;
+        if (localizedText.ContainsKey (key)) 
+        {
+            result = localizedText [key];
+        }
+
+        return result;
+
+    }
+
+    public bool GetIsReady()
+    {
+        return isReady;
+    }
+
+}
---
+++ Assets/Scripts/Localization/LocalizedText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour {
+
+    public string key;
+
+    // Use this for initialization
+    void Start () 
+    {
+        Text text = GetComponent<Text> ();
+        text.text = LocalizationManager.instance.GetLocalizedValue (key);
+    }
+
+}
