43452fc8492e4e29a5f5309f945159648a7e31ab
ReallyRad <arthur.pointeau@gmail.com>
2020-04-15 19:10:59 +0100

    scaffold familiarization timeline playback with video recording

--- Assets/ExperimentManager.cs
+++ Assets/ExperimentManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public enum ParticipantType { leader, follower };
 public enum ConditionType { control, experimental };
@@ -22,29 +23,43 @@ public class ExperimentManager : MonoBehaviour
     {
       if (instance == null) instance = this;
     }
-    
+
+    private void Start()
+    {
+    }
+
     public void StartExperiment(bool familiarization = false)
     {
-        _interventionTimeline.Play();
+        ExperimentSettingsGUI.instance.gameObject.SetActive(false); //disable experiment GUI
+        //TODO activate/deactivate clip tracks depending on if leader or follower
+        //TimelineAsset timelineAsset = (TimelineAsset) _interventionTimeline.playableAsset;
+        //var tracks = timelineAsset.GetOutputTracks(); ... etc 
+
+        if (!familiarization)
+        {
+            _interventionTimeline.Play();
         
-        if (condition == ConditionType.experimental)
-        {
-            OscManager.instance.sendHeadTracking = true; //enable sending/receiving headtracking
-            VideoFeed.instance.twoWayWap = true; //move POV according to other headtracking
-        }
-        else
-        {
-            VideoFeed.instance.twoWayWap = false; //POV follows own headtracking
-        }
+            if (condition == ConditionType.experimental)
+            {
+                OscManager.instance.sendHeadTracking = true; //enable sending/receiving headtracking
+                VideoFeed.instance.twoWayWap = true; //move POV according to other headtracking
+            }
+            else
+            {
+                VideoFeed.instance.twoWayWap = false; //POV follows own headtracking
+            }
         
-        if (participant == ParticipantType.follower)
-        {
-            //play follower free phase instruction audio or text
-        }
-        else
-        {
-            //play leader free phase instruction audio or text
-        }
+            if (participant == ParticipantType.follower)
+            {
+                //play follower free phase instruction audio or text
+            }
+            else
+            {
+                //play leader free phase instruction audio or text
+            }    
+        } else _familiarizationTimeline.Play();
+        
+        
     }
 
     public void StartFreePhase()
@@ -73,6 +88,8 @@ public class ExperimentManager : MonoBehaviour
     public void EndIntervention()
     {
         OscManager.instance.sendHeadTracking = false;
+        
+        Debug.Log("End of intervention");
     }
 
 }
