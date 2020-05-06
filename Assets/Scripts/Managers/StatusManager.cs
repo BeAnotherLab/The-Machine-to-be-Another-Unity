988ed5f6de01984a4aa548c0ec164ca885770f4c
ReallyRad <arthur.pointeau@gmail.com>
2020-05-06 19:58:31 +0100

    removed timing lists and replaced them by timelines. remove audio instructions from scene

--- Assets/Scripts/Managers/StatusManager.cs
+++ Assets/Scripts/Managers/StatusManager.cs
@@ -19,8 +19,6 @@ public class StatusManager : MonoBehaviour {
     public UserStatus selfStatus;
     public UserStatus otherStatus;
     
-    public List<List<float>> timingList = new List<List<float>>();
-    
     #endregion
 
 
@@ -31,21 +29,9 @@ public class StatusManager : MonoBehaviour {
     
     [Tooltip("Instructions Timing")] private GameObject _instructionsGUI;
 
-    [SerializeField]
-    private float waitBeforeInstructions; 
-    
-    [Tooltip("Set configuration of event timing (1-4)")] [SerializeField] private int _timingConfiguration;
-    private List<float> _selectedTimingList = new List<float>();
-    //TODO make waitafterInstructions match the duration of the introduction audio
-
     private GameObject _mainCamera;
 
     private GameObject _confirmationMenu;
-
-    [Tooltip("0 = instructions end, 1 = mirror, 2 = wall, 3 = goodbye")] [SerializeField] private List<float> timeList1 = new List<float>();
-    [Tooltip("0 = instructions end, 1 = mirror, 2 = wall, 3 = goodbye")] [SerializeField] private List<float> timeList2 = new List<float>();
-    [Tooltip("0 = instructions end, 1 = mirror, 2 = wall, 3 = goodbye")] [SerializeField] private List<float> timeList3 = new List<float>();
-    [Tooltip("0 = instructions end, 1 = mirror, 2 = wall, 3 = goodbye")] [SerializeField] private List<float> timeList4 = new List<float>();
     
     #endregion
 
@@ -57,13 +43,7 @@ public class StatusManager : MonoBehaviour {
         if (instance == null) instance = this;
 
         _mainCamera = GameObject.Find("Main Camera");
-
         _confirmationMenu = GameObject.Find("ConfirmationMenu");
-
-        timingList.Add(timeList1);
-        timingList.Add(timeList2);
-        timingList.Add(timeList3);
-        timingList.Add(timeList4);
     }
 
     private void Start()
@@ -89,10 +69,32 @@ public class StatusManager : MonoBehaviour {
 
     #region Public Methods
 
+    public void StartExperience()
+    {
+        if (_autoStartAndFinishOn) //if we are in auto swap
+        {
+            ArduinoManager.instance.SendCommand("wal_on"); //close curtain
+            ArduinoManager.instance.SendCommand("mir_off"); //hide mirror
+        }
+
+        if (_autoStartAndFinishOn) VideoFeed.instance.SetDimmed(false);
+        _instructionsGUI.SetActive(false);
+    }
+
+    public void MirrorOn()
+    {
+        ArduinoManager.instance.SendCommand("mir_on");
+    }
+
+    public void WallOn()
+    {
+        ArduinoManager.instance.SendCommand("wal_off"); //open curtain
+        ArduinoManager.instance.SendCommand("mir_off"); //hide mirror
+    }
+    
     public void SetAutoStartAndFinish(bool on, float waitTime = 5)
     {
         _autoStartAndFinishOn = on;
-        waitBeforeInstructions = waitTime;
     }
 
     public void ThisUserIsReady() //called when user has aimed at the confirmation dialog and waited through the countdown.
@@ -136,8 +138,6 @@ public class StatusManager : MonoBehaviour {
             VideoFeed.instance.SetDimmed(true);
 
             InstructionsTextBehavior.instance.ShowTextFromKey("otherIsGone");
-
-            StopAllCoroutines();
 
             StartCoroutine(WaitBeforeResetting()); //after a few seconds, reset experience.
         }
@@ -214,52 +214,6 @@ public class StatusManager : MonoBehaviour {
         InstructionsDisplay.instance.ShowWaitForTurnVideo();
     }
     
-    private IEnumerator StartPlayingCoroutine()
-    {      
-        if (_autoStartAndFinishOn) //if we are in auto swap
-        {
-            StartCoroutine("GoodbyeCoroutine");
-            AudioPlayer.instance.PlayAudioInstructions(_timingConfiguration);
-        }
-
-        StartCoroutine("MirrorCoroutine");
-        StartCoroutine("WallCoroutine");
-
-        yield return new WaitForFixedTime(waitBeforeInstructions);// wait before playing audio
-        yield return new WaitForFixedTime(_selectedTimingList[0]);//duration of audio track to start video after
-        
-        if (_autoStartAndFinishOn) //if we are in auto swap
-        {
-            ArduinoManager.instance.SendCommand("wal_on"); //close curtain
-            ArduinoManager.instance.SendCommand("mir_off"); //hide mirror
-        }
-
-        if (_autoStartAndFinishOn) VideoFeed.instance.SetDimmed(false);
-        _instructionsGUI.SetActive(false);
-    }
-
-    private IEnumerator GoodbyeCoroutine()
-    {
-        yield return new WaitForFixedTime(_selectedTimingList[3] + waitBeforeInstructions);
-        Debug.Log("READY TO STOP");
-        IsOver();
-    }
-
-    private IEnumerator MirrorCoroutine()
-    {
-        yield return new WaitForFixedTime(waitBeforeInstructions + _selectedTimingList[1]);
-        Debug.Log("READY FOR MIRROR");
-        ArduinoManager.instance.SendCommand("mir_on"); //show mirror
-    }
-
-    private IEnumerator WallCoroutine()
-    {
-        yield return new WaitForFixedTime(waitBeforeInstructions + _selectedTimingList[2]);
-        Debug.Log("READY FOR WALL");
-        ArduinoManager.instance.SendCommand("wal_off"); //open curtain
-        ArduinoManager.instance.SendCommand("mir_off"); //hide mirror
-    }
-    
     private void EnableConfirmationGUI(bool enable)
     {
         ConfirmationButton.instance.gameObject.SetActive(enable);
@@ -279,9 +233,7 @@ public class StatusManager : MonoBehaviour {
         {
             _instructionsGUI.SetActive(true);
             InstructionsTextBehavior.instance.ShowTextFromKey("instructions");
-            _selectedTimingList.Clear();
-            foreach (int item in timingList[_timingConfiguration]) _selectedTimingList.Add(item);
-            StartCoroutine("StartPlayingCoroutine");    
+            //TODO start playing back timeline
         }
     }
 
@@ -289,7 +241,6 @@ public class StatusManager : MonoBehaviour {
     {
         VideoFeed.instance.SetDimmed(true);
         InstructionsTextBehavior.instance.ShowTextFromKey("finished");
-        StopAllCoroutines();
     }
 
     private IEnumerator WaitBeforeResetting()
