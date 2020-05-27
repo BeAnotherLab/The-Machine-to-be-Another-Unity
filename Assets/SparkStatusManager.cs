a5f094a19c1fc6d894863033623e91d20dfcf0f4
ReallyRad <arthur.pointeau@gmail.com>
2020-05-27 19:16:10 +0100

    create spark swap  timeline, swap status

---
+++ Assets/SparkStatusManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SparkStatusManager : StatusManager
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void StartFreePhase()
+    {
+        
+    }
+
+    public void StartTactilePhase()
+    {
+        
+    }
+}
