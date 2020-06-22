319cb87c1d866eb16129bde433da383cc0999b23
ReallyRad <arthur.pointeau@gmail.com>
2020-06-22 12:50:43 +0100

    remove audio instructons, remove "instructed" phase from timeline

--- Assets/ExperimentManager.cs
+++ Assets/ExperimentManager.cs
@@ -11,31 +11,21 @@ public class ExperimentManager : MonoBehaviour
     [SerializeField] private PlayableDirector _interventionTimeline;
     [SerializeField] private VideoPlayer _videoPlayer;
 
-    [SerializeField] private TrackAsset _leaderTrack;
-    [SerializeField] private TrackAsset _followerTrack;
-
     [SerializeField] private ExperimentData _experimentData;
     
     private void Awake()
     {
       if (instance == null) instance = this;
-      OscManager.OnOtherStatus += StartExperiment;
     }
 
     private void Start()
     {
         TimelineAsset timelineAsset = (TimelineAsset) _interventionTimeline.playableAsset;
-        _followerTrack = timelineAsset.GetOutputTrack(3);
-        _leaderTrack = timelineAsset.GetOutputTrack(2);
         _experimentData.experimentState = ExperimentState.intervention;
     }
 
-    public void StartExperiment()
+    public void StartFreePhase()
     {
-        //activate/deactivate clip tracks depending on if leader or follower
-        _followerTrack.muted = _experimentData.participantType != ParticipantType.follower;
-        _leaderTrack.muted = _experimentData.participantType != ParticipantType.leader; 
-        
         _interventionTimeline.Play();
     
         if (_experimentData.conditionType == ConditionType.experimental)
@@ -43,30 +33,26 @@ public class ExperimentManager : MonoBehaviour
             OscManager.instance.SetSendHeadtracking(true); //enable sending/receiving headtracking
             VideoFeed.instance.twoWayWap = true; //move POV according to other headtracking
         }
-        else 
+        else if (_experimentData.conditionType == ConditionType.control)
         {
             VideoFeed.instance.twoWayWap = false; //POV follows own headtracking
-        }
-    
-        if (_experimentData.participantType == ParticipantType.follower && _experimentData.conditionType == ConditionType.control)
-        {
-            string filePath;
-            if (_experimentData.controlVideos.TryGetValue(_experimentData.subjectID, out filePath))
-                _videoPlayer.url = filePath;
-            else 
-                Debug.Log("file not found!");
-        }
-        
-    }
-
-    public void StartFreePhase()
-    {
-        //play free phase instruction audio or text
-        if (_experimentData.participantType == ParticipantType.follower && _experimentData.conditionType == ConditionType.control)
-        {
-            //switch to pre recorded video
-            VideoFeed.instance.ShowLiveFeed(false);
-            _videoPlayer.Play();
+            
+            if (_experimentData.participantType == ParticipantType.follower)
+            {
+                string filePath;
+                if (_experimentData.controlVideos.TryGetValue(_experimentData.subjectID, out filePath))
+                    _videoPlayer.url = filePath;
+                else 
+                    Debug.Log("file not found!");
+            }
+            
+            //play free phase instruction audio or text
+            else if (_experimentData.participantType == ParticipantType.follower)
+            {
+                //switch to pre recorded video
+                VideoFeed.instance.ShowLiveFeed(false);
+                _videoPlayer.Play();
+            }
         }
         
         Debug.Log("start free phase for " + _experimentData.conditionType + " " + _experimentData.participantType);
@@ -75,11 +61,9 @@ public class ExperimentManager : MonoBehaviour
     public void StartTactilePhase()
     {
         //play tactile phase instruction audio or text
-
         if (_experimentData.participantType == ParticipantType.follower && _experimentData.conditionType == ConditionType.control)
         {
-            //switch back to live video
-            VideoFeed.instance.ShowLiveFeed(true);
+            VideoFeed.instance.ShowLiveFeed(true); //switch back to live video
         }
         
         Debug.Log("start tactile phase for " + _experimentData.conditionType + " " + _experimentData.participantType);
