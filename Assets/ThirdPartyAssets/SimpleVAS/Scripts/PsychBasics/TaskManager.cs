f93daba23cc27c2473ecae2e43d3d29278dbd31b
specsdhdeveloper <arthur.pointeau@gmail.com>
2020-06-23 18:29:58 +0200

    add regions, consistent variable prefixing

--- Assets/ThirdPartyAssets/SimpleVAS/Scripts/PsychBasics/TaskManager.cs
+++ Assets/ThirdPartyAssets/SimpleVAS/Scripts/PsychBasics/TaskManager.cs
@@ -8,41 +8,49 @@ namespace UnityPsychBasics {
 
     public class TaskManager : MonoBehaviour {
 
+        #region public variables
+        
         public Text textUI;
         public Button _nextButton;
         public Scrollbar _scrollbar;
         public ToggleGroup _toggleGroup;
         public Image _image;
 
-        [HideInInspector]
-        public bool shuffle, useImages, useAnalogueScale;
-
-        [HideInInspector]
-        public bool setValueOutside;
+        [HideInInspector] public bool shuffle, useImages, useAnalogueScale;
+        [HideInInspector] public bool setValueOutside;
 
         public static TaskManager instance;
 
-        private Timer _timer;
+        #endregion
+
+        
+        #region private variables
+        
         private ScaleManager _scaleManager;
 
-        private List<string> questionList = new List<string>();
-        private List<Sprite> imageList = new List<Sprite>();
-        private List<int> indexList = new List<int>();
-
-        private int currentItem;
-
+        private List<string> _questionList = new List<string>();
+        private List<Sprite> _imageList = new List<Sprite>();
+        private List<int> _indexList = new List<int>();
+        private int _currentItem;
+        
+        #endregion
+        
+        
+        #region MonoBehavior methods
+        
         private void Awake() {
             if (instance == null) instance = this;
         }
 
         void Start() {
-
             _nextButton.interactable = false;
-
-            _timer = Timer.instance;
-            _timer.stopwatch.Start();
-
+            Timer.instance.stopwatch.Start();
         }
+
+        #endregion
+
+
+        #region Public methods
 
         public void InitializeValuesListsAndObjects() {
 
@@ -64,7 +72,7 @@ namespace UnityPsychBasics {
             }
 
             if (useImages) {
-                for (int i = 0; i < ImageRead.instance.imageSprites.Count; i++) imageList.Add(ImageRead.instance.imageSprites[i]);
+                for (int i = 0; i < ImageRead.instance.imageSprites.Count; i++) _imageList.Add(ImageRead.instance.imageSprites[i]);
                 if (shuffle) CreateShuffleList();
                 SetImage();
             }
@@ -73,12 +81,57 @@ namespace UnityPsychBasics {
                 CsvRead.instance.SetFileToLoad();
 
                 for (int i = 0; i < CsvRead.instance.questionnaireInput.Count; i++)
-                    questionList.Add(CsvRead.instance.questionnaireInput[i]);
+                    _questionList.Add(CsvRead.instance.questionnaireInput[i]);
 
                 if (shuffle) CreateShuffleList();
-                textUI.text = questionList[currentItem];
+                textUI.text = _questionList[_currentItem];
             }
         }
+        
+        public void OnResponseSelection() {
+            _nextButton.interactable = true;
+        }
+
+        public void OnNextButton() {
+            CsvWrite.instance.responseTime = Timer.instance.ElapsedTimeAndRestart();
+            _nextButton.interactable = false;
+            CsvWrite.instance.item = _currentItem;
+            if (!setValueOutside) CsvWrite.instance.response = ResponseValue();
+            CsvWrite.instance.LogTrial();
+            _scrollbar.value = 0.5f;
+            DoAfterSeletion();
+        }
+
+        public float ResponseValue() {
+            float currentValue = 0;
+
+            if (!useAnalogueScale) {
+                Toggle[] numberOfToggles = _toggleGroup.GetComponentsInChildren<Toggle>();
+
+                for (int i = 0; i < numberOfToggles.Length; i++)
+                    if (numberOfToggles[i].isOn)
+                        currentValue = i;
+
+                _toggleGroup.SetAllTogglesOff();
+                _nextButton.interactable = false;
+            }
+            else currentValue = _scrollbar.value;
+
+            return currentValue;
+        }
+
+        public void OutsideResponseValue(float outsideValue){
+            CsvWrite.instance.response = outsideValue;
+        }
+
+        public void LoadScene(string scene) {
+            SceneManager.LoadScene(scene);
+        }
+        
+        #endregion
+
+
+        #region Private methods
 
         private void ShowGameObjects(GameObject[] objectToShow) //TODO use Canvas Grop instead
         {
@@ -97,122 +150,69 @@ namespace UnityPsychBasics {
         private void CreateShuffleList(){
 
             if(useImages)
-                for (int i = 0; i < imageList.Count; i++)
-                    indexList.Add(i);
+                for (int i = 0; i < _imageList.Count; i++)
+                    _indexList.Add(i);
             else
-                for (int i = 0; i < questionList.Count; i++)
-                    indexList.Add(i);
+                for (int i = 0; i < _questionList.Count; i++)
+                    _indexList.Add(i);
 
-            currentItem = ShuffleValue();
+            _currentItem = ShuffleValue();
         }
 
         private int ShuffleValue() {
 
-            int randomIndex = Random.Range(0, indexList.Count);
-            int selectedItem = indexList[randomIndex];
-            indexList.RemoveAt(randomIndex);
+            int randomIndex = Random.Range(0, _indexList.Count);
+            int selectedItem = _indexList[randomIndex];
+            _indexList.RemoveAt(randomIndex);
 
             return selectedItem;
         }
 
-
-        public void OnResponseSelection() {
-            _nextButton.interactable = true;
-        }
-
-        public void OnNextButton() {
-            CsvWrite.instance.responseTime = _timer.ElapsedTimeAndRestart();
-            _nextButton.interactable = false;
-            CsvWrite.instance.item = currentItem;
-            if (!setValueOutside) CsvWrite.instance.response = ResponseValue();
-            CsvWrite.instance.LogTrial();
-            _scrollbar.value = 0.5f;
-            DoAfterSeletion();
-        }
-
-        public float ResponseValue() {
-            float currentValue = 0;
-
-                if (!useAnalogueScale) {
-                    Toggle[] numberOfToggles = _toggleGroup.GetComponentsInChildren<Toggle>();
-
-                    for (int i = 0; i < numberOfToggles.Length; i++)
-                        if (numberOfToggles[i].isOn)
-                            currentValue = i;
-
-                    _toggleGroup.SetAllTogglesOff();
-                    _nextButton.interactable = false;
-                }
-                else currentValue = _scrollbar.value;
-
-            return currentValue;
-        }
-
-        public void OutsideResponseValue(float outsideValue){
-            CsvWrite.instance.response = outsideValue;
-        }
+        #endregion
 
         private void DoAfterSeletion(){
-
             if (!shuffle) {
-                currentItem++;
+                _currentItem++;
 
                 if (useImages) {
-                    if (currentItem < imageList.Count)
-                        SetImage();
-
-                    else if (currentItem == imageList.Count)
-                        QuestionsExhausted();
-
-                    _timer.stopwatch.Start();
+                    if (_currentItem < _imageList.Count) SetImage();
+                    else if (_currentItem == _imageList.Count) QuestionsExhausted();
+                    Timer.instance.stopwatch.Start();
                 }
 
                 else {
-                    if (currentItem < questionList.Count)
-                        textUI.text = questionList[currentItem];
-
-                    else if (currentItem == questionList.Count)
-                        QuestionsExhausted();
+                    if (_currentItem < _questionList.Count) textUI.text = _questionList[_currentItem];
+                    else if (_currentItem == _questionList.Count) QuestionsExhausted();
                 }
             }
 
             else {
-                if (indexList.Count != 0) {
-                    currentItem = ShuffleValue();
-
-                    if (useImages)
-                        SetImage();
-
-                    else
-                        textUI.text = questionList[currentItem];
-
-                    _timer.stopwatch.Start();
+                if (_indexList.Count != 0) {
+                    _currentItem = ShuffleValue();
+                    if (useImages) SetImage();
+                    else textUI.text = _questionList[_currentItem];
+                    Timer.instance.stopwatch.Start();
                 }
-
-                else if (indexList.Count == 0)
-                    QuestionsExhausted();
+                else if (_indexList.Count == 0) QuestionsExhausted();
             }
         }
 
         private void SetImage(){
-            
-            _image.sprite = imageList[currentItem];
+            _image.sprite = _imageList[_currentItem];
             _image.GetComponent<RectTransform>().sizeDelta = new Vector2(_image.sprite.rect.width, (float)_image.sprite.rect.height);
         }
 
-
         private void QuestionsExhausted() {
-
             CsvWrite.instance.condition++;
-            currentItem = 0;
-            questionList.Clear();
-            imageList.Clear();
-            indexList.Clear();
+            _currentItem = 0;
+            _questionList.Clear();
+            _imageList.Clear();
+            _indexList.Clear();
 
             _nextButton.interactable = false;
             
             if (TaskSettings.instance == null) {
-                UnityEngine.Debug.Log("You must attach the LoadSceneAfterTask object somewhere in the scene and add Scene names to it");//else the call is ambiguous for the diagnostics library
+                Debug.Log("You must attach the LoadSceneAfterTask object somewhere in the scene and add Scene names to it");//else the call is ambiguous for the diagnostics library
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
 
@@ -225,10 +225,6 @@ namespace UnityPsychBasics {
             }
         }
 
-        public void LoadScene(string scene) {
-            SceneManager.LoadScene(scene);
-        }
-
     }
 
-    }
+}
