f294c591873bd45e1dac11baddbaad13446fe0d9
specsdhdeveloper <arthur.pointeau@gmail.com>
2020-06-23 18:54:04 +0200

    clean up a bit

--- Assets/ThirdPartyAssets/SimpleVAS/Scripts/PsychBasics/ScaleManager.cs
+++ Assets/ThirdPartyAssets/SimpleVAS/Scripts/PsychBasics/ScaleManager.cs
@@ -8,72 +8,44 @@ namespace UnityPsychBasics
 
         public GameObject togglePrefab;
         public ToggleGroup toggleGroup;
-
         public GameObject scrollbar;
 
-        [HideInInspector]
-        public string minVASLabel, midVASLabel, maxVASLabel;
-
-        [HideInInspector]
-        public List<string> likertItems = new List<string>();
-
-        private TaskManager _taskManager;
-        private LabelNames _labelNames;
+        [HideInInspector] public string minVASLabel, midVASLabel, maxVASLabel;
+        [HideInInspector] public List<string> likertItems = new List<string>();
 
         public static ScaleManager instance;
 
         private void Awake()
         {
-            if (instance == null)
-                instance = this;
-
-            _taskManager = TaskManager.instance;
-            _labelNames = LabelNames.instance;
+            if (instance == null) instance = this;
         }
 
         public void CreateToggles(){
 
-            foreach(Transform child in toggleGroup.GetComponent<Transform>()){
-                GameObject.Destroy(child.gameObject);
-            }
+            foreach(Transform child in toggleGroup.GetComponent<Transform>())
+                Destroy(child.gameObject);
 
-            if (!_taskManager.useAnalogueScale)
+            if (!TaskManager.instance.useAnalogueScale)
                 for (int i = 0; i < likertItems.Count; i++)
-                    CreateToggle(i);
+                {
+                    var instanciatedPrefab = Instantiate(togglePrefab, Vector3.zero, Quaternion.identity);
+
+                    instanciatedPrefab.transform.SetParent(toggleGroup.gameObject.transform, false);
+
+                    Toggle toggle = instanciatedPrefab.GetComponent<Toggle>();
+                    toggle.GetComponentInChildren<Text>().text = likertItems[i];
+                    toggle.group = toggleGroup;
+                    toggle.onValueChanged.AddListener(delegate {TaskManager.instance.OnResponseSelection(); });
+                }
 
             else
-                SetAnalogueScaleNames();
-        }
-
-        private void CreateToggle(int index) {
-
-            GameObject _instanciatedPrefab = Instantiate(togglePrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-            _instanciatedPrefab.transform.SetParent(toggleGroup.gameObject.transform, false);
-
-            Toggle _toggle = _instanciatedPrefab.GetComponent<Toggle>();
-
-            _toggle.GetComponentInChildren<Text>().text = likertItems[index];
-
-             _toggle.group = toggleGroup;
-
-            _toggle.onValueChanged.AddListener(delegate { ToggleAction(_toggle); });
-        }
-
-        private void SetAnalogueScaleNames(){
-            foreach (Transform child in scrollbar.transform){
-                if (child.name == "Left label")
-                    child.GetComponent<Text>().text = minVASLabel;
-                else if (child.name == "Middle label")
-                    child.GetComponent<Text>().text = midVASLabel;
-                else if (child.name == "Right label")
-                    child.GetComponent<Text>().text = maxVASLabel;
+            {
+                foreach (Transform child in scrollbar.transform){
+                    if (child.name == "Left label") child.GetComponent<Text>().text = minVASLabel;
+                    else if (child.name == "Middle label") child.GetComponent<Text>().text = midVASLabel;
+                    else if (child.name == "Right label") child.GetComponent<Text>().text = maxVASLabel;
+                }
             }
-
-        }
-        
-        private void ToggleAction(Toggle changed){
-            _taskManager.OnResponseSelection();
         }
     }
 }
