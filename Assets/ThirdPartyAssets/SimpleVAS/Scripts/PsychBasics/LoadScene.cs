79b03752e23ba8f273fd15bfd88aebdda7d9341c
ReallyRad <arthur.pointeau@gmail.com>
2020-06-24 13:54:21 +0100

    clean up loadscene

--- Assets/ThirdPartyAssets/SimpleVAS/Scripts/PsychBasics/LoadScene.cs
+++ Assets/ThirdPartyAssets/SimpleVAS/Scripts/PsychBasics/LoadScene.cs
@@ -5,33 +5,23 @@ using UnityEngine.SceneManagement;
 
 namespace UnityPsychBasics
 {
-	
-public class LoadScene : MonoBehaviour {
-
-	public string sceneToLoad;
-	public bool changeOnKey;
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
-		if (changeOnKey)
-			if (Input.GetKeyDown ("space"))
-				OnNextButton ();
+	public class LoadScene : MonoBehaviour {
+
+		public string sceneToLoad;
+		public bool changeOnKey;
+
+		// Update is called once per frame
+		void Update () 
+		{
+			if (Input.GetKeyDown ("space") && changeOnKey) OnNextButton ();
+		}
+
+		public void OnNextButton () 
+		{
+			if (sceneToLoad != "") SceneManager.LoadScene (sceneToLoad);
+			else SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		} 
 	}
-
-
-	public void OnNextButton () {
-
-		if (sceneToLoad != "")
-			SceneManager.LoadScene (sceneToLoad);
-		else
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
-	} 
-}
 
 }
