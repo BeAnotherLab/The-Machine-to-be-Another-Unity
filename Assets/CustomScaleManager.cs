5a998bccb41317f2a41ec19ba94dbff656682a9d
ReallyRad <arthur.pointeau@gmail.com>
2020-06-24 16:07:38 +0100

    also make custom scale manager and task settings. add them to scene

---
+++ Assets/CustomScaleManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityPsychBasics
+{
+    public class CustomScaleManager : MonoBehaviour 
+    {
+        public GameObject togglePrefab;
+        public ToggleGroup toggleGroup;
+        public GameObject scrollbar;
+
+        [HideInInspector] public string minVASLabel, midVASLabel, maxVASLabel;
+        [HideInInspector] public List<string> likertItems = new List<string>();
+
+        public static CustomScaleManager instance;
+
+        private void Awake()
+        {
+            if (instance == null) instance = this;
+        }
+
+        public void CreateToggles()
+        {
+            foreach(Transform child in toggleGroup.GetComponent<Transform>())
+                Destroy(child.gameObject);
+
+            if (!CustomTaskManager.instance.useAnalogueScale)
+                for (int i = 0; i < likertItems.Count; i++)
+                {
+                    var instanciatedPrefab = Instantiate(togglePrefab, Vector3.zero, Quaternion.identity);
+
+                    instanciatedPrefab.transform.SetParent(toggleGroup.gameObject.transform, false);
+
+                    Toggle toggle = instanciatedPrefab.GetComponent<Toggle>();
+                    toggle.GetComponentInChildren<Text>().text = likertItems[i];
+                    toggle.group = toggleGroup;
+                    toggle.onValueChanged.AddListener(delegate {CustomTaskManager.instance.OnResponseSelection(); });
+                }
+
+            else
+            {
+                foreach (Transform child in scrollbar.transform) 
+                {
+                    if (child.name == "Left label") child.GetComponent<Text>().text = minVASLabel;
+                    else if (child.name == "Middle label") child.GetComponent<Text>().text = midVASLabel;
+                    else if (child.name == "Right label") child.GetComponent<Text>().text = maxVASLabel;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/CustomTaskSettings.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityPsychBasics
+{
+    public class CustomTaskSettings : MonoBehaviour 
+    {
+
+        public string sceneBeforeLastCondition, sceneAfterLastCondition;
+
+        public static CustomTaskSettings instance;
+
+        public int currentTask;
+
+        public bool withinScene, shuffleBool, useImageBool, useAnalogueScaleBool;
+
+        public string minVASLabel, midVASLabel, maxVASLabel;
+        public List<string> likertItems = new List<string>();
+
+        public List<bool> shuffle = new List<bool>();
+        public List<bool> useImage = new List<bool>();
+        public List<bool> analogueScale = new List<bool>();
+        public List<bool> useMouseClickSelector = new List<bool>();
+
+        private void Awake() 
+        {
+            if (instance == null) instance = this;
+        }
+
+        private void Start() 
+        {
+            if (withinScene) SetWithinScene(false);
+            else { //set for separate scenes
+                MouseClickResponse.instance.ActivateSelector(shuffleBool);
+                CustomTaskManager.instance.useImages = useImageBool;
+                CustomTaskManager.instance.useAnalogueScale = useAnalogueScaleBool;
+                CustomTaskManager.instance.shuffle = shuffleBool;
+                CustomTaskManager.instance.InitializeValuesListsAndObjects();
+            } 
+
+            ScaleManager.instance.minVASLabel = minVASLabel;
+            ScaleManager.instance.midVASLabel = midVASLabel;
+            ScaleManager.instance.maxVASLabel = maxVASLabel;
+
+            for (int i = 0; i < likertItems.Count; i++) 
+                ScaleManager.instance.likertItems.Add(likertItems[i]);
+        }
+
+        public void LoadBeforeLast() 
+        {
+            if (!withinScene) CustomTaskManager.instance.LoadScene(sceneBeforeLastCondition);
+            else SetWithinScene(false); 
+        }
+
+        public void LoadAfterLast() 
+        {
+            if (!withinScene) CustomTaskManager.instance.LoadScene(sceneAfterLastCondition);
+            else SetWithinScene(true);
+        }
+        
+        private void SetWithinScene(bool isLast) 
+        {
+            if (currentTask < useImage.Count) {
+                MouseClickResponse.instance.ActivateSelector(useMouseClickSelector[currentTask]);
+                CustomTaskManager.instance.useImages = useImage[currentTask];
+                CustomTaskManager.instance.useAnalogueScale = analogueScale[currentTask];
+                CustomTaskManager.instance.shuffle = shuffle[currentTask];
+                CustomTaskManager.instance.InitializeValuesListsAndObjects();
+                currentTask++;
+            }
+            else {
+                if (!isLast) CustomTaskManager.instance.LoadScene(sceneBeforeLastCondition);
+                else CustomTaskManager.instance.LoadScene(sceneAfterLastCondition);
+            }
+        }
+    }
+}
