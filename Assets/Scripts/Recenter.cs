3f93147cf662189e12bf4ec37172064c4f1018a2
ReallyRad <arthur.pointeau@gmail.com>
2020-09-14 18:38:45 +0100

    Don't reset like a dumbass

--- Assets/Scripts/Recenter.cs
+++ Assets/Scripts/Recenter.cs
@@ -7,10 +7,13 @@ public class Recenter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("n")) UnityEngine.XR.InputTracking.Recenter();
-        //The following will also move the camera positional reference.
-        //taken from https://forum.unity.com/threads/openvr-how-to-reset-camera-properly.417509/#post-2792972
-        Valve.VR.OpenVR.System.ResetSeatedZeroPose();
-        Valve.VR.OpenVR.Compositor.SetTrackingSpace(Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
+        if (Input.GetKeyDown("n"))
+        {
+            UnityEngine.XR.InputTracking.Recenter();
+            //The following will also move the camera positional reference.
+            //taken from https://forum.unity.com/threads/openvr-how-to-reset-camera-properly.417509/#post-2792972
+            Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+            Valve.VR.OpenVR.Compositor.SetTrackingSpace(Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
+        }
     }
 }
