959be46f7c01b16e2f34d25a757d2d001ad2307d
ReallyRad <arthur.pointeau@gmail.com>
2020-09-14 17:35:17 +0100

    also use vive recenter method in cstom recenter script

--- Assets/Scripts/Recenter.cs
+++ Assets/Scripts/Recenter.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Recenter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("n")) UnityEngine.XR.InputTracking.Recenter();
+        //The following will also move the camera positional reference.
+        //taken from https://forum.unity.com/threads/openvr-how-to-reset-camera-properly.417509/#post-2792972
+        Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+        Valve.VR.OpenVR.Compositor.SetTrackingSpace(Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
     }
 }
