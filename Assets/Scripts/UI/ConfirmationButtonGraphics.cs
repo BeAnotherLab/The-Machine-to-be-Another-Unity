c3d518331c982bbfb1cac32f8d19951a3d62210c
marteroel <marteroel@users.noreply.github.com>
2020-09-25 19:01:56 +0200

    Added animation to confirmation button

--- Assets/Scripts/UI/ConfirmationButtonGraphics.cs
+++ Assets/Scripts/UI/ConfirmationButtonGraphics.cs
@@ -13,15 +13,31 @@ public class ConfirmationButtonGraphics : MonoBehaviour
         if (instance == null) instance = this;
     }
 
+    private void Start()
+    {
+        StartCoroutine(AnimateButton());
+    }
+
 
     public void SwitchSelection(bool onOff) {
         if (onOff) {
             this.gameObject.GetComponent<MeshRenderer>().material = buttonOn;
-            Debug.Log("confirmation on");
+            StopCoroutine(AnimateButton());
             }
+
         else {
             this.gameObject.GetComponent<MeshRenderer>().material = buttonOff;
-            Debug.Log("confirmation off");
+            StartCoroutine(AnimateButton());
         }
+    }
+
+    private IEnumerator AnimateButton() {
+        yield return new WaitForSeconds(0.6f);
+        LeanTween.scale(gameObject, new Vector3(1.1f, 1.1f, 1.1f), 0.5f).setEaseOutBounce();
+
+        yield return new WaitForSeconds(0.6f);
+        
+        LeanTween.scale(gameObject, new Vector3(1f, 1f, 1f), 0.5f).setEaseOutBounce();
+        StartCoroutine(AnimateButton());
     }
 }
