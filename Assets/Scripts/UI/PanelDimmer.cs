0e1bb7afb56d06a0a6a4431ea59b5de5a30356ca
Arthur Pointeau <arthur.pointeau@gmail.com>
2021-12-01 18:40:33 +0100

    add panel dimmer script. Get ready to show questionnaire

---
+++ Assets/Scripts/UI/PanelDimmer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelDimmer : MonoBehaviour
+{
+    public void Show(bool show, float opacity, float time = 0.7f, bool loop = false, float delay = 0f)
+    {
+        var from = GetComponent<CanvasGroup>().alpha;
+        var to = 0f;
+        if (show) to = opacity;
+        
+        var loopType = LeanTweenType.once;
+        if (loop) loopType = LeanTweenType.pingPong;
+
+        if(!loop) GetComponent<CanvasGroup>().blocksRaycasts = show;
+        
+        LeanTween.value(gameObject, from, to, time )
+            .setOnUpdate(delegate(float val)
+            {
+                GetComponent<CanvasGroup>().alpha = val;
+            })
+            .setLoopCount(2)
+            .setLoopType(loopType)
+            .setDelay(delay)
+            .setEaseOutCubic();
+    }
+    
+}
