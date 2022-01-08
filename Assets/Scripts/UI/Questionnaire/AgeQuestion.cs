c589ddf80d33da5afa87ff909a250e509f743bd6
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-01-07 20:20:59 -0500

    display age on scrollbar update. it's rounded to intervals of 10 for some reason

---
+++ Assets/Scripts/UI/Questionnaire/AgeQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AgeQuestion : MonoBehaviour
+{
+    [SerializeField] private Text _labelText;
+    
+    public void SetText(float value)
+    {
+        _labelText.text = Mathf.RoundToInt(value * 100).ToString();
+    }
+}
