6c2ef470a0281a7757a2e212947dc45c576b2a16
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-01-20 12:30:41 +0100

    move scrollbar with gaze

--- Assets/Scripts/UI/Questionnaire/QuestionnaireVRSlider.cs
+++ Assets/Scripts/UI/Questionnaire/QuestionnaireVRSlider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using ScriptableObjectArchitecture;
 using UnityEngine;
@@ -6,15 +7,17 @@ using UnityEngine.UI;
 using UnityEngine.XR;
 using VRStandardAssets.Utils;
 
-public class QuestionnaireVRButton : MonoBehaviour
+public class QuestionnaireVRSlider : MonoBehaviour
 {
-    //TODO use static events instead of direct reference.
     [SerializeField] private VRInteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
-
-    [SerializeField] private Button _button;
     [SerializeField] private GameEvent _showSelectionRadialEvent;
     [SerializeField] private GameEvent _hideSelectionRadialEvent;
+    [SerializeField] private CanvasGroup _canvasGroup;
     
+    private Scrollbar _scrollbar;
+    private GameObject _sliderHandle;
+    private Transform reticlePosition;
+
     private bool m_GazeOver;                                            // Whether the user is looking at the VRInteractiveItem currently.
 
     private void OnEnable()
@@ -29,14 +32,21 @@ public class QuestionnaireVRButton : MonoBehaviour
         m_InteractiveItem.OnOut -= HandleOut;
     }
 
+    private void Awake()
+    {
+        _sliderHandle = GetComponent<Scrollbar>().handleRect.gameObject;
+        _scrollbar = GetComponent<Scrollbar>();
+        _canvasGroup = transform.parent.transform.parent.GetComponent<CanvasGroup>();
+        reticlePosition = Camera.main.gameObject.GetComponent<Reticle>().ReticleTransform;
+    }
+
     private void HandleOver()
     {
+        Debug.Log("handle over");
         // When the user looks at the rendering of the scene, show the radial.
-        if (XRDevice.userPresence == UserPresenceState.Present && GetComponent<Button>().interactable)
+        if (XRDevice.userPresence == UserPresenceState.Present && _canvasGroup.alpha == 1)
         {
             _showSelectionRadialEvent.Raise();
-            LeanTween.scale(gameObject, new Vector3(1.2f, 1.2f, 1.2f), 0.45f).setEaseOutBounce();
-            //LeanTween.color(gameObject, Color.white, 0.25f).setEaseOutCubic();
             m_GazeOver = true;
         }
     }
@@ -45,15 +55,18 @@ public class QuestionnaireVRButton : MonoBehaviour
     {
         // When the user looks away from the rendering of the scene, hide the radial.
         _hideSelectionRadialEvent.Raise();
-        LeanTween.scale(gameObject, new Vector3(1, 1, 1), 0.45f).setEaseOutBounce();
-        //LeanTween.color(gameObject, Color.gray, 0.25f).setEaseOutCubic();
         m_GazeOver = false;
     }
 
     public void HandleSelectionComplete()
     {
-        if (m_GazeOver) 
-            _button.onClick.Invoke();
+        if (m_GazeOver) {
+            float scrollBarSize = GetComponent<RectTransform>().rect.width;
+            Vector3 relativeToCanvas = GetComponentInParent<Canvas>().gameObject.transform.InverseTransformPoint(reticlePosition.transform.position);
+            float mappedPosition =(relativeToCanvas.x/(scrollBarSize))+0.5f;       
+            _sliderHandle.SetActive(true);
+            _scrollbar.value = mappedPosition;
+        }
         HandleOut();            
     }
 }
