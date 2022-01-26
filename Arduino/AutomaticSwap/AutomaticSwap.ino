c9afb5a15f80214dd6d7d55c34b45b4687749736
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-01-26 16:47:49 +0100

    something had gone wrong

--- Arduino/AutomaticSwap/AutomaticSwap.ino
+++ Arduino/AutomaticSwap/AutomaticSwap.ino
@@ -1,66 +1,70 @@
 #include<Uduino.h>
 Uduino uduino("serialControl");
 
-//Define pins
-int stopPin = 4;
-int relayPin = 5;
 int step = 9;
 int dir = 8;
-
-
 int stepsPerRev = 200;
 int numTurns = 2;
 int numTurnsHoming = 1;
-int delayTime = 750; 
+int delayTime = 750; //us
 
-int stopState;
+int relayPin = 5;
+int eStopPin = 4;
+int eStopState;
 bool homing = false;
 int steps = 0;
 
 
-void setup() {
+void setup() { 
   Serial.begin(9600);
   
   pinMode(step, OUTPUT);
   pinMode(dir, OUTPUT);
   pinMode(relayPin, OUTPUT);
-  pinMode(stopPin, INPUT_PULLUP);
+  pinMode(eStopPin, INPUT_PULLUP);
 
   uduino.addCommand("wallTo", wallTo);  
-  uduino.addCommand("init", doHoming);
+  uduino.addCommand("homing", doHoming);
+
+  delay(1000);
+  //doHoming();
 }
 
 void loop() {
+  
+  /*wallTo(9000);
+  Serial.println(steps);
+  wallTo(1000);
+  Serial.println(steps);
+  doHoming();
+  Serial.println(steps);*/
   uduino.readSerial();
   delay(10);
 }
 
 void doHoming(){
+
+  Serial.println("cmd_ok");  //command executed
   
   delayTime = 1500;
 
-  //Look for curtain 0
+  //look for estop
   brakeOFF();
   homing=false;
   digitalWrite(dir,HIGH); //UP
-  
-  while(!homing) {
-    stopState = digitalRead(stopPin);
-    if(stopState == 0) 
-    {
-      homing = true;
-    }
-    else 
-    {
+  while(!homing){
+    eStopState = digitalRead(eStopPin);
+    if(eStopState==0){
+      homing=true;
+    }else{
        moveMotor();
     }
   }
 
-  //go to zero
+  //goto zero
   digitalWrite(dir,LOW); //DOWN
 
-  for(int i = 0; i<stepsPerRev*numTurnsHoming; i++)
-  {
+  for(unsigned long i=0;i<stepsPerRev*numTurnsHoming;i++){
     moveMotor();
   }
 
@@ -68,8 +72,9 @@ void doHoming(){
   delayTime = 750;
   steps = 0;
 
-  Serial.println("sys_rdy");  //command executed
+  //Serial.println("homing_ok");  //command executed
 }
+
 
 void wallTo(){
 
@@ -82,18 +87,16 @@ void wallTo(){
     stepTo = uduino.charToInt(uduino.getParameter(0)); 
   }
 
-  if(stepTo == -1) return;
+  if(stepTo==-1) return;
   
   int d;
   
-  if(stepTo>steps)
-  {
+  if(stepTo>steps){
     digitalWrite(dir,LOW); //DOWN
     d = 1;
   }
   
-  if(stepTo<steps)
-  {
+  if(stepTo<steps){
     digitalWrite(dir,HIGH); //UP
     d = 0;
   }
@@ -102,28 +105,21 @@ void wallTo(){
   Serial.println(stepsToMove);
   brakeOFF();
   
-  for(int i = 0; i<stepsToMove; i++)
-  {
-    if(steps>=11000)
-    {
+  for(int i=0;i<stepsToMove;i++){
+    if(steps>=11000){
       Serial.println("steps_error");  //command error
       break;
     }
-    
     moveMotor();
-
-    if(d==0)
-    {
+    if(d==0){
       steps--;
     }
-    else if(d==1)
-    {
+    if(d==1){
       steps++;
     }
   }
-
   brakeON(); 
-} 
+}
 
 void moveMotor(){
   digitalWrite(step,HIGH);
@@ -135,8 +131,10 @@ void moveMotor(){
 void brakeOFF(){
   digitalWrite(relayPin,LOW);
   delay(100);
+
 }
 
 void brakeON(){
   digitalWrite(relayPin,HIGH);
+
 }
