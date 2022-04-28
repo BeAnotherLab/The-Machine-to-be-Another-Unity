f6442705d7652fb686b528ae20674ef5adbd3743
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-04-28 18:44:20 +0200

    separate consent ui from user state

---
+++ Assets/Scripts/UI/ConsentUIAutoBodySwapLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class ConsentUIAutoBodySwapLogic : MonoBehaviour
+{
+    [SerializeField] private UserStateVariable _previousOtherState;
+
+    public void SelfStateChanged(UserState selfState)
+    {
+        //if self is now ready to start 
+        if (selfState == UserState.readyToStart) GetComponent<ConsentUI>().Show(true);
+        else if (selfState == UserState.headsetOff) GetComponent<ConsentUI>().Show(false);
+    }
+
+    public void OtherStateChanged(UserState otherState)
+    {
+        if(otherState == UserState.headsetOff && _previousOtherState == UserState.readyToStart) 
+            GetComponent<ConsentUI>().Show(true);
+    }
+}
--- Assets/ThirdPartyAssets/Questionnaire/Scripts/UI/ConsentUI.cs
+++ Assets/ThirdPartyAssets/Questionnaire/Scripts/UI/ConsentUI.cs
@@ -12,38 +12,17 @@ public class ConsentUI : MonoBehaviour
     [SerializeField] private GameObject _textPanel;
     [SerializeField] private Text _text;
     
-    [SerializeField] private UserStateVariable _previousOtherState;
-
-    public void SelfStateChanged(UserState selfState)
-    {
-        //if self is now ready to start 
-        if (selfState == UserState.readyToStart)
-        {
-            Show(true);
-        } 
-        else if (selfState == UserState.headsetOff)
-        {
-            Show(false);
-        }
-    }
-
-    public void OtherStateChanged(UserState otherState)
-    {
-        if(otherState == UserState.headsetOff && _previousOtherState == UserState.readyToStart) Show(false);
-    }
-    
     public void ReadyToShowQuestionnaire()
     {
         Show(false);
     }
-    
     
     public void ConsentButtonPressed()
     {
         _text.gameObject.GetComponent<LeanLocalizedText>().TranslationName = "waitForOther";
     }
     
-    private void Show(bool show)
+    public void Show(bool show)
     {
         _yesButton.gameObject.SetActive(show); 
         _noButton.gameObject.SetActive(show);
