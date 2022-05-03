5342a16f005c7f7e4f564df64cb4f2754930a282
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-05-03 17:57:38 +0200

    separate consent ui  from lean localization

---
+++ Assets/Scripts/UI/ConsentUIText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Localization;
+using UnityEngine.UI;
+
+public class ConsentUIText : MonoBehaviour //TODO use inheritence over composition
+{
+    [SerializeField] private Text _text;
+
+    public void ConsentButtonPressed()
+    {
+        _text.gameObject.GetComponent<LeanLocalizedText>().TranslationName = "waitForOther";
+    }
+
+    public void Show()
+    {
+        _text.gameObject.GetComponent<LeanLocalizedText>().TranslationName = "consent"; 
+    }
+}
