b60e525166621439bb6b90d9efb1cd59445bda95
Arthur <arthur.pointeau@gmail.com>
2022-05-15 18:58:13 +0200

    autobodyswap manager is just an implementation of the base class without modifications

---
+++ Assets/Scripts/Managers/AutoBodySwapStatusManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using ScriptableObjectArchitecture;
+using Uduino;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using Debug = DebugFile;
+using UnityEngine.XR;
+using VRStandardAssets.Utils;
+
+public  class AutoBodySwapStatusManager : StatusManager
+{
+
+  
+}
