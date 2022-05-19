ce9401ce900834841f5d00cf4c4b3dadb49199cf
Marte Roel <marteroel@users.noreply.github.com>
2022-05-19 08:36:49 -0800

    wallOn and wallOff commands

--- Arduino/AutomaticSwapKonstanz/AutomaticSwapKonstanz.ino
+++ Arduino/AutomaticSwapKonstanz/AutomaticSwapKonstanz.ino
@@ -1,30 +1,32 @@
 #include<Uduino.h>
+
 Uduino uduino("serialControl");
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
 
-//Pin definitions
-int stopPin = 4;
-int relayPin = 5;
-int dir = 8;
-int step = 9;
+const int eStop = 11;
+int eStopState;
 
-//Constants
-int wallOn = 10000; //curtain bottom position
-int wallOff = 40; //curtain top position
-int stepsPerRev = 200; //number of steps to go to after homing
-int delayTime = 750; //the time beween digital writes when moving the curtain
+const int pinENA = 6;
+const int pinIN1 = 3;
+const int pinIN2 = 4;
 
-//State variables
-int stopState; //course end sensor value
-int steps = 0;
+const int pinMotorA[3] = { pinENA, pinIN1, pinIN2 };
+
+bool goingUp = false;
+bool goingDown = false;
+
+unsigned long actualTime;
+unsigned long timeDown = 2000;
 
 void setup() 
 {
-  Serial.begin(9600);
-  
-  pinMode(step, OUTPUT);
-  pinMode(dir, OUTPUT);
-  pinMode(relayPin, OUTPUT);
-  pinMode(stopPin, INPUT_PULLUP);
+  Serial.begin(115200);
+ 
+  pinMode(eStop, INPUT_PULLUP);
+  pinMode(pinIN1, OUTPUT);
+  pinMode(pinIN2, OUTPUT);
+  pinMode(pinENA, OUTPUT);
 
   uduino.addCommand("wallOn", wallOnHandler);  
   uduino.addCommand("wallOff", wallOffHandler);   
@@ -41,33 +43,6 @@ void loop()
 void doHoming(bool confirm)
 {
   Serial.println("cmd_ok");  //command executed
-  
-  delayTime = 1500;
-
-  //Move up until reaching the top
-  brakeOFF();
-  bool homing = false;
-  
-  digitalWrite(dir,HIGH); //UP
-  
-  while (!homing)
-  {
-    stopState = digitalRead(stopPin);  
-    if (stopState==0) homing = true;
-    else moveMotor();
-  }
-
-  //Go to initial position
-  digitalWrite(dir,LOW); //DOWN
-
-  for (unsigned long i=0; i < stepsPerRev + 40; i++)
-  {
-    moveMotor();
-  }
-
-  brakeON();
-  delayTime = 750;
-  steps = 0;
 
   if (confirm)
   {
@@ -81,73 +56,42 @@ void initHandler(){
 
 void wallOnHandler() 
 {
-  wallHandler(true);
+  Serial.println("cmd_ok");  //command executed
+  moveForward(pinMotorA, 180); //move down at minimum power
+  delay(600); //()
+  //Serial.println("cmd_ok");  //command executed
+  fullStop(pinMotorA); //stop motor
 }
-
 
 void wallOffHandler()
 {
-  doHoming(false);
-}
-
-void wallHandler(bool on){
   Serial.println("cmd_ok");  //command executed
-
-  int stepTo;
-  
-  if (on) stepTo = wallOn;  
-  else stepTo = wallOff;  
-    
-  if (stepTo == -1) return;
-  int d;
-  
-  if (stepTo>steps)
-  {
-    digitalWrite(dir,LOW); //DOWN
-    d = 1;
+  while(digitalRead(eStop)==0){ //until we reach the top
+    moveBackward(pinMotorA, 255); //move up at full speed
   }
-  
-  else if (stepTo < steps)
-  {
-    digitalWrite(dir,HIGH); //UP
-    d = 0;
-  }
-
-  int stepsToMove = abs(stepTo - steps);
-  brakeOFF();
-  
-  for (int i=0; i<stepsToMove; i++)
-  {
-    if(steps >= 11000)
-    {
-      Serial.println("steps_error");  //command error
-      break;
-    }
-    
-    moveMotor();
-    
-    if (d==0) steps--;
-    if (d==1) steps++;
-  }
-  
-  brakeON(); 
+  //move back down a little
+  moveForward(pinMotorA, 180); //second parameter is speed (0-255)
+  delay(400);
+  fullStop(pinMotorA); //stop motor
 }
 
-void moveMotor()
+void moveForward(const int pinMotor[3], int speed) //go down
 {
-  digitalWrite(step,HIGH);
-  delayMicroseconds(delayTime);
-  digitalWrite(step,LOW);
-  delayMicroseconds(delayTime);
-} 
-
-void brakeOFF()
-{
-  digitalWrite(relayPin,LOW);
-  delay(100);
+   digitalWrite(pinMotor[1], HIGH);
+   digitalWrite(pinMotor[2], LOW);
+   analogWrite(pinMotor[0], speed);
 }
 
-void brakeON()
+void moveBackward(const int pinMotor[3], int speed) //go up
 {
-  digitalWrite(relayPin,HIGH);
+   digitalWrite(pinMotor[1], LOW);
+   digitalWrite(pinMotor[2], HIGH);
+   analogWrite(pinMotor[0], speed);
+}
+
+void fullStop(const int pinMotor[3])
+{
+   digitalWrite(pinMotor[1], LOW);
+   digitalWrite(pinMotor[2], LOW);
+   analogWrite(pinMotor[0], 0);
 }
