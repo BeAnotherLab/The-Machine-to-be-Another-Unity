6b3cab3f06bd0fd3d51e835d0421518da732c501
Arthur <arthur.pointeau@gmail.com>
2022-08-19 11:55:48 +0200

    copy manual status manager for auto status manager

--- Assets/Scripts/Managers/AutoBodySwapStatusManager.cs
+++ Assets/Scripts/Managers/AutoBodySwapStatusManager.cs
@@ -11,8 +11,35 @@ using Debug = DebugFile;
 using UnityEngine.XR;
 using VRStandardAssets.Utils;
 
-public class AutoBodySwapStatusManager : StatusManager 
+public class AutoBodySwapStatusManager : StatusManager //TODO redundant with CurtainManualSwapManager
 {
 
-  
+    public new void SelfStateChanged(UserState newState) //TODO move to own state changes events class
+    {
+        if (newState == UserState.headsetOff) SelfRemovedHeadset();
+        else if (newState == UserState.headsetOn) SelfPutHeadsetOn();
+        else if (newState == UserState.readyToStart)
+        {
+            OscManager.instance.SendThisUserStatus((UserState.readyToStart));
+            _languageButtons.gameObject.SetActive(false); //hide language buttons;
+
+            if (otherState.Value == UserState.readyToStart) StartPlaying(); //TODO this should be the default behavior
+        
+            _setInstructionsTextGameEvent.Raise("waitForOther");
+            Debug.Log("this user is ready", DLogType.Input);
+        }
+    }
+
+    public new void OtherStateChanged(UserState newState)
+    {
+        if (newState == UserState.headsetOff) OtherLeft();
+        else if (newState == UserState.headsetOn) OtherPutHeadsetOn(); //TODO only if previous one was ready to start?
+        else if (newState == UserState.readyToStart) OtherUserIsReady();
+    }
+    
+    public void OtherUserIsReady()
+    {
+        Debug.Log("the other user is ready", DLogType.Input);
+        if (selfState.Value == UserState.readyToStart) StartPlaying();//TODO this should be the default behavior
+    }
 }
