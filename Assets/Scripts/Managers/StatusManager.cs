b4d7c8a8505d5844a9c22a4e3eb83200516e89a2
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-11-15 17:22:01 +0100

    reset status manager

--- Assets/Scripts/Managers/StatusManager.cs
+++ Assets/Scripts/Managers/StatusManager.cs
@@ -1,20 +1,23 @@
-﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using ScriptableObjectArchitecture;
-using Uduino;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.XR;
+using UnityEngine.UI;
+using VRStandardAssets.Menu;
+using VRStandardAssets.Utils;
+using Uduino;
+using UnityEngine.Serialization;
 using UnityEngine.Timeline;
 using Debug = DebugFile;
-using UnityEngine.XR;
-using VRStandardAssets.Utils;
 
-public abstract class StatusManager : MonoBehaviour
-{
+
+public class StatusManager : MonoBehaviour {
+
     #region Public Fields
 
-    public static StatusManager instance; //TODO remove
+    public static StatusManager instance;
 
     public bool presenceDetection; //TODD check if still necessary
     
@@ -30,60 +33,61 @@ public abstract class StatusManager : MonoBehaviour
     public PlayableDirector instructionsTimeline;
     
     #endregion
-    
-    #region Protected Fields
-    
-    [SerializeField] protected PlayableDirector _shortTimeline; //TODO shouldn't be in abstract status manager 
-    [SerializeField] protected PlayableDirector _longTimeline; //TODO shouldn't be in abstract status manager
-    [SerializeField] protected GameObject _languageButtons;
 
-    [SerializeField] protected GameEvent _standbyGameEvent;
-    [SerializeField] protected GameEvent _InstructionsStartedGameEvent;
-    [SerializeField] protected BoolGameEvent _experienceFinishedGameEvent;
-    [SerializeField] protected GameEvent _experienceStartedGameEvent;
-    [SerializeField] protected StringGameEvent _languageChangeEvent;
-    [SerializeField] protected BoolGameEvent _curtainOnEvent;
-    
-    [SerializeField] protected StringGameEvent _setInstructionsTextGameEvent;
-    [SerializeField] protected BoolGameEvent _showInstructionsTextGameEvent;
 
-    [SerializeField] protected TrackAsset _germanTrack;
-    [SerializeField] protected TrackAsset _englishTrack;
+    #region Private Fields
     
-    protected GameObject _confirmationMenu; //TODO use events, no direct reference!
-    protected bool _experienceRunning;
-    protected bool _dimOutOnExperienceStart;
+    [SerializeField] private PlayableDirector _shortTimeline;
+    [SerializeField] private PlayableDirector _longTimeline;
+    [SerializeField] private GameObject _languageButtons;
+
+    [SerializeField] private GameEvent _standbyGameEvent;
+    [SerializeField] private GameEvent _InstructionsStartedGameEvent;
+    [SerializeField] private BoolGameEvent _experienceFinishedGameEvent;
+    [SerializeField] private GameEvent _experienceStartedGameEvent;
+    [SerializeField] private StringGameEvent _languageChangeEvent;
+    [SerializeField] private BoolGameEvent _curtainOnEvent;
     
-    protected delegate void OnShowTimedText(string key, int time);
-    protected OnShowTimedText ShowTimedText;
+    [SerializeField] private StringGameEvent _setInstructionsTextGameEvent;
+    [SerializeField] private QuestionnaireStateVariable _questionnaireState;
+
+    [SerializeField] private TrackAsset _germanTrack;
+    [SerializeField] private TrackAsset _englishTrack;
+    
+    private GameObject _mainCamera;
+    private bool _readyForStandby; //when we use serial, only go to standby if Arduino is ready.
+    private GameObject _confirmationMenu;
+    private bool _experienceRunning;
+    private bool _dimOutOnExperienceStart;
+    private bool _showQuestionnaireThisRound;
     
     #endregion
 
-    #region Monobehaviour Methods
-    
-    protected void Awake()
+
+    #region MonoBehaviour Methods
+
+    private void Awake()
     {
         if (instance == null) instance = this;
 
-        _confirmationMenu = GameObject.Find("ConfirmationMenu"); //TOOD don't use references like that
-        UduinoManager.Instance.OnBoardDisconnectedEvent.AddListener(delegate {
-            //SerialFailure(); //TODO wait for a few seconds for reconnection instead of going staight to failure
-        });
+        _mainCamera = GameObject.Find("Main Camera");
+        _confirmationMenu = GameObject.Find("ConfirmationMenu");
+        UduinoManager.Instance.OnBoardDisconnectedEvent.AddListener(delegate { SerialFailure(); });
         instructionsTimeline = _longTimeline; //use short experience by default
     }
-
-    // Start is called before the first frame update
-    protected void Start()
+    
+    private void Start()
     {
         if (SwapModeManager.instance.ArduinoControl)
             _setInstructionsTextGameEvent.Raise("serial");
+        else
+            _readyForStandby = true; //if we're not using the serial control, we don't have to wait for the arduino
 
         selfState.Value = UserState.headsetOff;
         otherState.Value = UserState.headsetOff;
     }
 
-    // Update is called once per frame
-    protected void Update()
+    private void Update()
     {
         if (XRDevice.userPresence == UserPresenceState.NotPresent && selfState.Value != UserState.headsetOff)
         {
@@ -100,29 +104,19 @@ public abstract class StatusManager : MonoBehaviour
             
         if (Input.GetKeyDown("o")) IsOver();
     }
-    
+
     #endregion
-    
-    #region Public Methods 
-    
+
+
+    #region Public Methods
+
     public void StartExperience() //TODO remove?
     {
-        _showInstructionsTextGameEvent.Raise(false);
+        InstructionsTextBehavior.instance.ShowInstructionText(false);
         if (_dimOutOnExperienceStart) VideoFeed.instance.Dim(false);
         else instructionsTimeline.Stop();
         _experienceStartedGameEvent.Raise();
         Debug.Log("experience started");
-    }
-    
-    public void SerialFailure() //if something went wrong with the physical installation
-    {
-        VideoFeed.instance.Dim(true);
-        AudioManager.instance.StopAudioInstructions();    
-        _setInstructionsTextGameEvent.Raise("systemFailure");
-        instructionsTimeline.Stop();
-        _experienceRunning = false;
-        Destroy(gameObject);
-        Debug.Log("serial failure", DLogType.Error);
     }
 
     public void MirrorOn()
@@ -133,27 +127,40 @@ public abstract class StatusManager : MonoBehaviour
 
     public void CloseWall()
     {
+        ArduinoManager.instance.WallOn(true);
+        //ArduinoManager.instance.SendCommand("mir_off"); //hide mirror
         Debug.Log("wall on");        
         _curtainOnEvent.Raise(true);
     }
     
     public void WallOn() //TODO rename
     {
+        OpenCurtainCanvasController.instance.Show("Open Curtain");
+        ArduinoManager.instance.WallOn(false);
         _curtainOnEvent.Raise(false);
         //ArduinoManager.instance.SendCommand("mir_off"); //hide mirror
         Debug.Log("wall off");
     }
 
-    protected void ThisUserIsReady() //called when user has aimed at the confirmation dialog and waited through the countdown.
+    public void ThisUserIsReady() //called when user has aimed at the confirmation dialog and waited through the countdown.
     {
         OscManager.instance.SendThisUserStatus(UserState.readyToStart);
+
+        //EnableConfirmationGUI(false); //hide status confirmation GUI elements
         _languageButtons.gameObject.SetActive(false); //hide language buttons;
-        _setInstructionsTextGameEvent.Raise("waitForOther");
+
+        //start experience or wait for the other if they're not ready yet
+        //if (otherState.Value == UserState.readyToStart) StartPlaying();
+        //InstructionsTextBehavior.instance.ShowTextFromKey("waitForOther");
+
+        InstructionsTextBehavior.instance.ShowInstructionText(false);
+        
         Debug.Log("this user is ready", DLogType.Input);
     }
 
     public void OtherUserIsReady()
     {
+        //if (selfState.Value == UserState.readyToStart) StartPlaying();
         Debug.Log("the other user is ready", DLogType.Input);
     }
 
@@ -168,64 +175,98 @@ public abstract class StatusManager : MonoBehaviour
     {
         Debug.Log("the other user put on the headset", DLogType.Input);
     }
-
+    
     public void OtherLeft()
     {
         //if experience started
         if (previousOtherState.Value == UserState.readyToStart)
         {
             //only reset on other left if experience running, post finished, or doing pre questionnaire
-            if (_experienceRunning) 
+            if (_experienceRunning || _questionnaireState.Value != QuestionnaireState.post) 
             {
                 instructionsTimeline.Stop();
-                _experienceRunning = false;
-                _setInstructionsTextGameEvent.Raise("otherIsGone");
+                _experienceRunning = false;    
                 StartCoroutine(WaitBeforeResetting()); //after a few seconds, reset experience.
                 selfState.Value = UserState.headsetOn;    
             }
         }
         Debug.Log("the other user removed the headset", DLogType.Input);
     }
-    
+
     public void Standby(bool start = false, bool dimOutOnExperienceStart = true)
     {
-        Debug.Log("Standby");
         if (!start) VideoFeed.instance.Dim(true); //TODO somehow this messes with Video Feed dimming when called on Start?
-        _setInstructionsTextGameEvent.Raise("idle");
+            _setInstructionsTextGameEvent.Raise("idle");
 
         instructionsTimeline.Stop();
         _experienceRunning = false;
+        _showQuestionnaireThisRound = false;
         
         AudioManager.instance.StopAudioInstructions();
 
-        InstructionsTextBehavior.instance.gameObject.GetComponent<FadeController>().FadeInText(); //TODO use events instead of static reference
-        InstructionsTextBehavior.instance.gameObject.GetComponent<FadeController>().FadeOutImages();  //TODO use events instead of static reference
+        InstructionsTextBehavior.instance.gameObject.GetComponent<FadeController>().FadeInText();
+        InstructionsTextBehavior.instance.gameObject.GetComponent<FadeController>().FadeOutImages();
         
+        //reset user status as it is not ready
+        //EnableConfirmationGUI(true);
+        _languageChangeEvent.Raise("German");  //reset to German by default
         _languageButtons.gameObject.SetActive(true); //show language buttons;
 
+        if (_readyForStandby) //TODO is check necessary? 
+            ArduinoManager.instance.InitialPositions();
+        
         Debug.Log("ready to start");
         
-        VideoFeed.instance.Dim(true); //TODO use events instead of static reference
+        VideoFeed.instance.Dim(true);
 
         _dimOutOnExperienceStart = dimOutOnExperienceStart;
         Debug.Log("setting dimOutOnExperienceStat to " + _dimOutOnExperienceStart);
         
         _standbyGameEvent.Raise();
     }
+    
+    public void EnablePresenceDetection(bool enablePresenceDetection)
+    {
+        
+    }
+    
+    public void SerialFailure() //if something went wrong with the physical installation
+    {
+        VideoFeed.instance.Dim(true);
+        OscManager.instance.SendSerialStatus(false);
+        AudioManager.instance.StopAudioInstructions();    
+        _setInstructionsTextGameEvent.Raise("systemFailure");
+        instructionsTimeline.Stop();
+        _experienceRunning = false;
+        Destroy(gameObject);
+        Debug.Log("serial failure", DLogType.Error);
+    }
+
+    public void SerialReady(bool serialControlComputer = false)
+    {
+        if (serialControlComputer) //if this computer is the one connected to the Arduino board
+        {
+            ArduinoManager.instance.InitialPositions();
+        }
+        
+        _setInstructionsTextGameEvent.Raise("idle");
+        _readyForStandby = true;
+        Debug.Log("serial ready", DLogType.System);
+    }    
 
     public void SelfRemovedHeadset()
     {
-        //TODO use event instead 
         _confirmationMenu.GetComponent<VRInteractiveItem>().Out(); //notify the VR interactive element that we are not hovering any more
-        if (previousSelfState.Value == UserState.readyToStart) {
+        if (previousSelfState.Value == UserState.readyToStart 
+            && _questionnaireState.Value != QuestionnaireState.post) { //reset unless doing post questionnaire
             Standby(false, _dimOutOnExperienceStart); //if we were ready and we took off the headset go to initial state
         }
         
-        OscManager.instance.SendThisUserStatus(selfState); //TODO use events instead
+        OscManager.instance.SendThisUserStatus(selfState);
         Debug.Log("this user removed his headset", DLogType.Input);
     }
-
-    public void SetInstructionsTimeline(int index) //TODO remove?
+    
+    public void SetInstructionsTimeline(int index)
     {
         if (index == 0)
             instructionsTimeline = _shortTimeline;
@@ -233,18 +274,30 @@ public abstract class StatusManager : MonoBehaviour
             instructionsTimeline = _longTimeline;
     }
 
-    public void SelfStateChanged(UserState newState) //TODO move to own state changes events class
+    public void SelfStateChanged(UserState newState)
     {
         if (newState == UserState.headsetOff) SelfRemovedHeadset();
         else if (newState == UserState.headsetOn) SelfPutHeadsetOn();
         else if (newState == UserState.readyToStart) ThisUserIsReady();
     }
 
-    public void OtherStateChanged(UserState newState) //TODO move to own state changes events class
+    public void OtherStateChanged(UserState newState)
     {
         if (newState == UserState.headsetOff) OtherLeft();
         else if (newState == UserState.headsetOn) OtherPutHeadsetOn(); //TODO only if previous one was ready to start?
         else if (newState == UserState.readyToStart) OtherUserIsReady();
+    }
+    
+    public void OnBothConsentsGiven(bool bothGiven)
+    {
+        _showQuestionnaireThisRound = bothGiven;
+        if(!bothGiven) StartPlaying();
+    }
+
+    public void OnBothQuestionnaireFinished(QuestionnaireState state)
+    {
+        if (state == QuestionnaireState.pre) StartPlaying();
+        else if (state == QuestionnaireState.post) StartCoroutine(WaitBeforeResetting()); 
     }
     
     public void SwitchLanguageTrack(string language)
@@ -256,21 +309,33 @@ public abstract class StatusManager : MonoBehaviour
         _germanTrack.muted = language != "German";
     }
 
-    
     #endregion
-    
-    #region Protected Methods
 
-    protected void IsOver() //called at the the end of the experience
+
+    #region Private Methods
+
+    private void StartPlaying()
+    {
+        if (_readyForStandby)
+        {
+            OpenCurtainCanvasController.instance.Show("Close Curtain");
+            instructionsTimeline.Play();
+            _InstructionsStartedGameEvent.Raise();
+            _experienceRunning = true;
+        }
+    }
+
+    private void IsOver() //called at the the end of the experience
     {
         VideoFeed.instance.Dim(true);
         //InstructionsTextBehavior.instance.ShowTextFromKey("finished");
         instructionsTimeline.Stop();
-        Debug.Log("experience finished");
+		Debug.Log("experience finished");
         _experienceRunning = false;
+        _experienceFinishedGameEvent.Raise(_showQuestionnaireThisRound);
     }
-    
-    protected IEnumerator WaitBeforeResetting()
+
+    private IEnumerator WaitBeforeResetting()
     {
         yield return new WaitForSeconds(4f); //make sure this value is inferior or equal to the confirmation radial time to avoid bugs
         Standby(false, _dimOutOnExperienceStart); //if we were ready and we took off the headset go to initial state
@@ -278,13 +343,6 @@ public abstract class StatusManager : MonoBehaviour
         Debug.Log("about to reset");
     }
 
-    protected void StartPlaying()
-    {
-        instructionsTimeline.Play();
-        _InstructionsStartedGameEvent.Raise();
-        _experienceRunning = true;
-    }
-
     #endregion
-    
 }
+ 
