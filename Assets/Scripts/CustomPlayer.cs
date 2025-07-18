9c4157a8855a2326b64ad43ed080ce776cc4bfde
ReallyRad <arthur.pointeau@gmail.com>
2025-07-18 20:25:23 +0200

    baseline network manager stuff? (not tested)

---
+++ Assets/Scripts/CustomPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+namespace Mirror.Examples.Pong
+{
+    public class CustomPlayer : NetworkBehaviour
+    {
+        private GameObject _mainCamera;
+        private GameObject _videoFeedFlipParent;
+        
+        private void Awake()
+        {
+            _mainCamera = GameObject.Find("Main Camera");
+            _videoFeedFlipParent = GameObject.Find("VideoFeedFlipParent");
+        }
+
+        private void Start()
+        {
+            if (!isLocalPlayer)
+            {
+                transform.SetParent(_videoFeedFlipParent.transform, false);
+                VideoFeed.instance.targetTransform = transform;
+                gameObject.name = "remote player";
+            }
+            else
+            {
+                gameObject.name = "local player";
+            }
+            StartCoroutine(VideoFeed.instance.StartupDim());
+            transform.GetChild(0).transform.localRotation = Quaternion.Euler(0,0, PlayerPrefs.GetFloat("tiltAngle"));
+        }
+
+        // need to use FixedUpdate for rigidbody
+        private void Update()
+        {
+            // only let the local player control the racket.
+            // don't control other player's rackets
+            if (isLocalPlayer)
+            {
+                transform.rotation = _mainCamera.transform.rotation;
+                GetComponentInChildren<MeshRenderer>().enabled = false;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Managers/CustomNetworkManager.cs
+++ Assets/Scripts/Managers/CustomNetworkManager.cs
@@ -18,30 +18,13 @@ namespace Mirror.Examples.Pong
             
         public bool offlineMode;
         
-        //Data collection consent values and events 
-        [SerializeField] private BoolGameEvent bothConsentGiven; //TODO move to a simple int field in this class
-        private int _consentCount; //how many times consent answers were given
-        [SerializeField] private IntVariable _consentsGiven; //how many positive answers were given
-
-        //Pre/Post questionnaires end events 
-        [SerializeField] private QuestionnaireStateGameEvent _bothQuestionnairesFinishedRpcEvent;
-        private int _questionnairesPreFinishedCount;
-        private int _questionnairesPostFinishedCount;
-
-        //video data collection consent
-        [SerializeField] private IntVariable _videoConsentsGiven; //how many positive answers were given TODO move to a simple int field
-        
-        [SerializeField] private BoolVariable _sendRecordingCommand;
-
         private void OnEnable()
         {
-            CustomPlayer.VideoConsentGivenCmd += VideoConsentAnswerGiven;
             DisplayManager.SetDisplayModeEvent += EnableNetworkGUI;
         }
 
         private void OnDisable()
         {
-            CustomPlayer.VideoConsentGivenCmd -= VideoConsentAnswerGiven;
             DisplayManager.SetDisplayModeEvent -= EnableNetworkGUI;
         }
 
@@ -53,8 +36,6 @@ namespace Mirror.Examples.Pong
         private void Start()    
         {
             if (offlineMode) Instantiate(playerPrefab);
-            _consentsGiven.Value = 0;
-            _videoConsentsGiven.Value = 0; //In standby instead?
             networkAddress = PlayerPrefs.GetString("othersIP");
 
             if (PlayerPrefs.GetInt("repeater", 0) == 1) //TODO rename property
@@ -65,12 +46,6 @@ namespace Mirror.Examples.Pong
 
         public void OnStandby()
         {
-            if (_consentsGiven.Value > 0) _consentsGiven.Value = 0;
-            if (_videoConsentsGiven.Value > 0) _videoConsentsGiven.Value = 0;
-            _sendRecordingCommand.Value = false;
-            if (_consentCount > 0) _consentCount = 0;
-            _questionnairesPreFinishedCount = 0;
-            _questionnairesPostFinishedCount = 0;
         }
         
         public override void OnServerAddPlayer(NetworkConnection conn)
@@ -91,43 +66,6 @@ namespace Mirror.Examples.Pong
             base.OnServerDisconnect(conn);
         }
 
-        public void ConsentAnswerGiven(bool consent)
-        {
-            _consentCount++;
-            if (consent) _consentsGiven.Value++;
-            if (_consentCount == 2)
-            {
-                if (_consentsGiven.Value == 2) bothConsentGiven.Raise(true);
-                else bothConsentGiven.Raise(false);
-            }
-        }
-        
-        public void QuestionnaireFinished(QuestionnaireState state)
-        {
-            if (state == QuestionnaireState.pre)
-            {
-                _questionnairesPreFinishedCount++;
-                if (_questionnairesPreFinishedCount == 2)
-                {
-                    _bothQuestionnairesFinishedRpcEvent.Raise(QuestionnaireState.pre);
-                }
-            }
-            else if (state == QuestionnaireState.post)
-            {
-                _questionnairesPostFinishedCount++;
-                if (_questionnairesPostFinishedCount == 2)
-                {
-                    _bothQuestionnairesFinishedRpcEvent.Raise(QuestionnaireState.post);
-                }
-            }
-        }
-        
-        private void VideoConsentAnswerGiven(bool consent)
-        {
-            if (consent) _videoConsentsGiven.Value++;
-            _sendRecordingCommand.Value = _videoConsentsGiven.Value == 2;
-        }
-        
         private IEnumerator TryConnect()
         {
             while (!NetworkClient.isConnected)
