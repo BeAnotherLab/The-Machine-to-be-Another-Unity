a13dc76334b4f562d7d11d68c748a18a70e2d490
Daniel González <arthur.pointeau@gmail.com>
2025-07-31 15:32:48 +0200

    synchronized is the trigger state for vive pro 2

--- Assets/Scripts/Managers/StatusManager.cs
+++ Assets/Scripts/Managers/StatusManager.cs
@@ -96,7 +96,7 @@ public class StatusManager : MonoBehaviour
 
     protected void Update()
     {
-        if (SessionStateFeature.GetCurrentState() == (int) XrSessionState.Idle  && selfState.Value != UserState.headsetOff)
+        if (SessionStateFeature.GetCurrentState() == (int) XrSessionState.Synchronized  && selfState.Value != UserState.headsetOff)
         {
             previousSelfState.Value = selfState.Value;
             selfState.Value = UserState.headsetOff; 
