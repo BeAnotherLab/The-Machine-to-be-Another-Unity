c5faee1b771ffa9e060fb05ed88b0c8eb7cac092
Arthur Pointeau <arthur.pointeau@protonmail.com>
2025-09-02 10:17:37 +0200

    monitor height

--- Assets/Scripts/HeightPresenceDetection.cs
+++ Assets/Scripts/HeightPresenceDetection.cs
@@ -10,13 +10,16 @@ public class HeightPresenceDetection : MonoBehaviour
     public delegate void OnHeadsetUnderThreshold();
     public static OnHeadsetUnderThreshold HeadsetUnderThreshold;
 
-    [Tooltip("Y position below which the headset is considered removed.")] [Range(0.2f, 2f)] [SerializeField]
+    [Tooltip("Y position below which the headset is considered removed.")] 
+    [Range(0.2f, 2f)] [SerializeField]
     private float _yThreshold;
 
+    [Range(0.2f, 2f)] [SerializeField]
     private float _previousHeight;
     
     private void Update() //Monitor VR headset height to infer user presence
     {
+        
         if (transform.position.y < _yThreshold && _previousHeight >= _yThreshold)  //we just passed under threshold
         {
             HeadsetUnderThreshold();
