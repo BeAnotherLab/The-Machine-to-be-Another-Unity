1b266de72fbf238d6aa140c85dea3387e0be9cd9
Arthur Pointeau <arthur.pointeau@protonmail.com>
2025-09-03 20:07:28 +0200

    rename

--- Assets/ThirdPartyAssets/VRUI/Scripts/ConfirmationButton.cs
+++ Assets/ThirdPartyAssets/VRUI/Scripts/ConfirmationButton.cs
@@ -14,10 +14,10 @@ namespace VRStandardAssets.Menu
         [SerializeField] private VRInteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
 
         //TODO separate the following to keep confirmation button pure VRUI stuff
-        [SerializeField] private UserStateGameEvent selfStateGameEvent;//TODO prepend "_" in name
-        [SerializeField] private UserStateVariable selfState; //TODO prepend "_" in name
+        [SerializeField] private UserStateGameEvent _selfStateGameEvent;
+        [SerializeField] private UserStateVariable _selfState;
         
-        public bool gazeOver;                                            // Whether the user is looking at the VRInteractiveItem currently.
+        public bool gazeOver; // Whether the user is looking at the VRInteractiveItem currently.
 
         private void OnEnable()
         {
@@ -38,8 +38,8 @@ namespace VRStandardAssets.Menu
             if (gazeOver) { //hide TODO use panel dimmer
                 GetComponent<MeshRenderer>().enabled = false;
                 GetComponent<MeshCollider>().enabled = false;
-                selfState.Value = UserState.readyToStart; //TODO separate
-                selfStateGameEvent.Raise(selfState.Value); //TODO separate
+                _selfState.Value = UserState.readyToStart; //TODO separate
+                _selfStateGameEvent.Raise(_selfState.Value); //TODO separate
             }
             HandleOut(); //deselect            
         }
@@ -47,7 +47,7 @@ namespace VRStandardAssets.Menu
         private void HandleOver() //TODO test
         {
             // When the user looks at the rendering of the scene, show the radial.
-            if (selfState.Value == UserState.headsetOn) //TODO separate
+            if (_selfState.Value == UserState.headsetOn) //TODO separate
             {
                 _showSelectionRadialEvent.Raise(true);
                 gazeOver = true;
