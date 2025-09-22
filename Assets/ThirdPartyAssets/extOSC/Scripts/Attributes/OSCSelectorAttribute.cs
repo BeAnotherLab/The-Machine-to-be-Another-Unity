a8a5e98dbfd6d5beb55d058f77422a698e18f8a3
Arthur Pointeau <arthur.pointeau@protonmail.com>
2025-09-22 18:52:57 +0200

    re-add ext OSC

---
+++ Assets/ThirdPartyAssets/extOSC/Scripts/Attributes/OSCSelectorAttribute.cs
@@ -0,0 +1,9 @@
+﻿/* Copyright (c) 2020 ExT (V.Sigalkin) */
+
+using UnityEngine;
+
+namespace extOSC
+{
+	public class OSCSelectorAttribute : PropertyAttribute
+	{ }
+}
---
+++ Assets/ThirdPartyAssets/extOSC/Scripts/Mapping/OSCMapEnum.cs
@@ -0,0 +1,32 @@
+﻿/* Copyright (c) 2020 ExT (V.Sigalkin) */
+
+namespace extOSC.Mapping
+{
+	public enum OSCMapType
+	{
+		Float,
+
+		Int,
+
+		FloatToBool,
+
+		BoolToFloat,
+
+		IntToBool,
+
+		BoolToInt
+	}
+
+	public enum OSCMapLogic
+	{
+		GreaterOrEquals,
+
+		Greater,
+
+		LessOrEquals,
+
+		Less,
+
+		Equals
+	}
+}
